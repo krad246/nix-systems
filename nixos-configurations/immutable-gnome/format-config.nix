cc972e1326cf3cd3cef92b44970e1ec9fb4f411f
Keerthi Radhakrishnan <krad246@gmail.com>
2024-08-05 17:30:03 -0400

    Figure out how to make a Disko config bind to a nixos-generator config

--- nixos-configurations/immutable-gnome/format-config.nix
+++ nixos-configurations/immutable-gnome/format-config.nix
@@ -1,6 +1,20 @@
 {
+  inputs,
+  self,
+  ...
+}: let
+  inherit (inputs) disko;
+  diskoLib = disko.lib;
+in {
   formatConfigs = {
     hyperv = _: {
+    };
+
+    raw-efi = _: {
+      system.build.raw = diskoLib.makeDiskImages {
+        nixosConfig = self.nixosConfigurations.immutable-gnome.extendModules {modules = [./fs-config/simple.nix];};
+        inherit diskoLib;
+      };
     };
   };
 }
