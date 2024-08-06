cfb768fc087f7f1afefb2eb90f8365e1fd57c293
Keerthi Radhakrishnan <krad246@gmail.com>
2024-08-06 15:57:44 -0400

    Implement some more format specializations

--- nixos-configurations/immutable-gnome/format-config.nix
+++ nixos-configurations/immutable-gnome/format-config.nix
@@ -15,11 +15,40 @@ in {
       boot.kernelParams = ["nomodeset"];
     };
 
+    install-iso = {modulesPath, ...}: {
+      imports = ["${modulesPath}/profiles/installation-device.nix"];
+      boot.supportedFilesystems = ["btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs"];
+    };
+
+    iso = _: {
+    };
+
+    qcow-efi = _: {
+    };
+
     raw-efi = _: {
       system.build.raw = diskoLib.makeDiskImages {
         nixosConfig = self.nixosConfigurations.immutable-gnome.extendModules {modules = [./fs-config/simple.nix];};
         inherit diskoLib;
       };
+    };
+
+    vagrant-virtualbox = _: {
+    };
+
+    virtualbox = _: {
+    };
+
+    vm-bootloader = _: {
+    };
+
+    vm-nogui = _: {
+    };
+
+    vm = _: {
+    };
+
+    vmware = _: {
     };
   };
 }
