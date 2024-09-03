5c9464e77af721926bd5e8c88cc71540ebdd3442
Keerthi Radhakrishnan <krad246@gmail.com>
2024-09-03 14:15:14 -0400

    just flake

--- build/just-flake.nix
+++ build/just-flake.nix
@@ -101,15 +101,19 @@
       commit = {
         enable = true;
         justfile = ''
-          commit MESSAGE +ARGS="": (add '-u')
-            ${lib.getExe pkgs.git} commit -m {{ quote(MESSAGE) }} {{ ARGS }}
+          commit MESSAGE="" +ARGS="": (add '-u')
+            ${lib.getExe pkgs.git} commit \
+              {{ if MESSAGE == "" { "" } \
+                  else { "-m" } }} \
+                    {{ MESSAGE }} \
+                    {{ ARGS }}
         '';
       };
 
       amend = {
         enable = true;
         justfile = ''
-          amend +ARGS="": (add '-A') (commit '--amend' ARGS)
+          amend +ARGS="": (add '-A') (commit "" "--amend" ARGS)
         '';
       };
     };
