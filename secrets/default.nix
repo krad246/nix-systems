3a75f2b489aa4a8afec0a12a513703cf74745d32
Keerthi Radhakrishnan <krad246@gmail.com>
2024-09-10 09:42:08 -0400

    Correct compile error in secrets

--- secrets/default.nix
+++ secrets/default.nix
@@ -1,41 +1,50 @@
 args @ {
   inputs,
-  config,
   lib,
   ...
 }: let
   inherit (inputs) agenix;
-  isHomeModule = lib.attrsets.hasAttrByPath ["osConfig"] args;
-  configSource =
-    if isHomeModule
-    then args.osConfig
-    else config;
+  hasHomeCtx = lib.attrsets.hasAttrByPath ["osConfig"] args;
+  hasSystemCtx = lib.attrsets.hasAttrByPath ["config"] args;
+  hasCtx = hasHomeCtx || hasSystemCtx;
+in
+  lib.attrsets.optionalAttrs hasCtx {
+    imports =
+      lib.optionals hasHomeCtx [agenix.homeManagerModules.age];
 
-  inherit (configSource.networking) hostName;
-in {
-  imports =
-    lib.optionals isHomeModule [agenix.homeManagerModules.age];
+    age = let
+      # determine if there is a supported host
+      configSrc =
+        if hasHomeCtx
+        then args.osConfig
+        else
+          (
+            if hasSystemCtx
+            then args.config
+            else {}
+          );
+      hasHostName = lib.attrsets.hasAttrByPath ["networking" "hostName"] configSrc;
 
-  age = let
-    # determine if there is a supported host
-    maybeHostDir = lib.fileset.maybeMissing ./hosts/${hostName};
+      maybeHostDir = lib.fileset.maybeMissing ./hosts/${configSrc.networking.hostName};
 
-    # pull in the secrets bound to the host
-    findAgeFiles = path: lib.fileset.fileFilter (file: file.hasExt "age") path;
-    ageFiles = findAgeFiles ./hosts;
-    hits = lib.fileset.toList (lib.fileset.intersection maybeHostDir ageFiles);
+      # pull in the secrets bound to the host
+      findAgeFiles = path: lib.fileset.fileFilter (file: file.hasExt "age") path;
+      ageFiles = findAgeFiles ./hosts;
+      hits = lib.fileset.toList (lib.fileset.intersection maybeHostDir ageFiles);
 
-    # for a given secret path, convert it into an agenix compatible attrset.
-    mkSecret = path: let
-      bname = builtins.baseNameOf path;
-      attrname = bname;
-    in {
-      "${attrname}" = {
-        file = path;
+      # for a given secret path, convert it into an agenix compatible attrset.
+      mkSecret = path: let
+        bname = builtins.baseNameOf path;
+        attrname = bname;
+      in {
+        "${attrname}" = {
+          file = path;
+        };
       };
-    };
-    hostSecrets = lib.lists.forEach hits mkSecret;
-  in {
-    secrets = lib.attrsets.mergeAttrsList hostSecrets;
-  };
-}
+
+      hostSecrets = lib.lists.forEach hits mkSecret;
+    in
+      lib.mkIf hasHostName {
+        secrets = lib.attrsets.mergeAttrsList hostSecrets;
+      };
+  }
