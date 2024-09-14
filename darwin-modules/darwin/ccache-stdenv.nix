3454ce346a8ccab4643a89304ae55e67983f2210
Keerthi Radhakrishnan <krad246@gmail.com>
2024-09-14 08:47:54 -0400

    Convert impure program refs

--- darwin-modules/darwin/ccache-stdenv.nix
+++ darwin-modules/darwin/ccache-stdenv.nix
@@ -1,20 +1,28 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   programs.ccache.enable = true;
 
   nixpkgs.overlays = [
     (_self: super: {
       ccacheWrapper = super.ccacheWrapper.override {
-        extraConfig = ''
+        extraConfig = let
+          echo = lib.getExe' pkgs.coreutils "echo";
+          sudo = lib.getExe pkgs.sudo;
+        in ''
           export CCACHE_COMPRESS=1
           export CCACHE_DIR="${config.programs.ccache.cacheDir}"
           export CCACHE_UMASK=007
           if [ ! -d "$CCACHE_DIR" ]; then
-            echo "====="
-            echo "Directory '$CCACHE_DIR' does not exist"
-            echo "Please create it with:"
-            echo "  sudo mkdir -m0770 '$CCACHE_DIR'"
-            echo "  sudo chown root:nixbld '$CCACHE_DIR'"
-            echo "====="
+            ${echo} "====="
+            ${echo} "Directory '$CCACHE_DIR' does not exist"
+            ${echo} "Please create it with:"
+            ${echo} "  ${sudo} ${lib.getExe' pkgs.coreutils "mkdir"} -m0770 '$CCACHE_DIR'"
+            ${echo} "  ${sudo} ${lib.getExe' pkgs.coreutils "chown"} root:nixbld '$CCACHE_DIR'"
+            ${echo} "====="
             exit 1
           fi
           if [ ! -w "$CCACHE_DIR" ]; then
