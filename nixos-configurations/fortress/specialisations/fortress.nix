a96784f6e261cec1a1f78a5a6bd9f6af11e3135f
Keerthi Radhakrishnan <krad246@gmail.com>
2024-09-18 19:31:48 -0400

    kernel steam not needed

--- nixos-configurations/fortress/specialisations/fortress.nix
+++ nixos-configurations/fortress/specialisations/fortress.nix
@@ -7,13 +7,6 @@
     ${config.networking.hostName} = {
       configuration = {
         imports = [ezModules.flatpak];
-
-        programs.steam = {
-          enable = true;
-          remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-          dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-          localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-        };
       };
     };
   };
