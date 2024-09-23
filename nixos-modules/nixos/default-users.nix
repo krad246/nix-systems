261472c1abf6ad4252d139671e877cbe8e1772ff
Keerthi Radhakrishnan <krad246@gmail.com>
2024-09-23 19:57:12 -0400

    remove passwd for default users

--- nixos-modules/nixos/default-users.nix
+++ nixos-modules/nixos/default-users.nix
@@ -1,13 +1,11 @@
-{lib, ...}: {
+{
   # Keep the stock users laying around.
   users = {
     users.nixos = {
-      initialHashedPassword = lib.mkDefault "$y$j9T$wGrPZQfqKmoKMYKh.DV2b.$I2/4NrxfRvS1d.M8QbPG.c8raX/jD8xBy.nOpQK./hA";
       isNormalUser = true;
     };
 
     users.root = {
-      initialHashedPassword = lib.mkDefault "$y$j9T$2CXtKDXh5W.N9aCUikvlM/$OmcgpG/XxMUK0KUx5Hh8/3CLUcSg3OFAK02fJQiw0a7";
       isSystemUser = true;
     };
   };
