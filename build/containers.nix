17e4e816a6f147cf6d111b20324196c43dafa5c2
Keerthi Radhakrishnan <krad246@gmail.com>
2024-10-19 20:59:24 -0400

    Rename devshell to loader, privatize loader to wrapper

--- build/containers.nix
+++ build/containers.nix
@@ -1,65 +1,10 @@
-{self, ...}: {
+_: {
   perSystem = {
     self',
-    config,
-    lib,
     pkgs,
     ...
   }: {
-    packages = lib.mkIf pkgs.stdenv.isLinux {
-      devshell = pkgs.dockerTools.streamLayeredImage {
-        name = "devshell";
-
-        fromImage = pkgs.dockerTools.pullImage {
-          imageName = "ubuntu";
-          imageDigest = "sha256:83f0c2a8d6f266d687d55b5cb1cb2201148eb7ac449e4202d9646b9083f1cee0";
-          sha256 = "sha256-5y6ToMw1UGaLafjaN69YabkjyCX61FT3QxU4mtmXMP0=";
-          finalImageName = "ubuntu";
-          finalImageTag = "latest";
-        };
-
-        contents = let
-          mkEnv = pkgs:
-            pkgs.buildEnv {
-              name = "devshell-contents";
-              paths =
-                # seems to be mandatory
-                [pkgs.dockerTools.binSh pkgs.bashInteractive]
-                ++ (with pkgs;
-                  [
-                    just
-                  ]
-                  ++ [
-                    fd
-                    ripgrep
-                    zoxide
-                  ]
-                  ++ [
-                    bat
-                    (bat-extras.batdiff.override {withDelta = true;})
-                    bat-extras.batgrep
-                  ]
-                  ++ [glow]
-                  ++ [direnv]
-                  ++ [starship]
-                  ++ [coreutils]
-                  ++ [cowsay hello]);
-
-              pathsToLink = ["/bin" "/share"];
-            };
-        in
-          mkEnv (
-            if pkgs.stdenv.isDarwin
-            then (import self.inputs.nixpkgs {system = "aarch64-linux";})
-            else pkgs
-          );
-
-        config = {
-          Env = [
-          ];
-        };
-      };
-
+    packages = {
       "docker/nix-shell" = pkgs.dockerTools.streamNixShellImage {
         drv = self'.devShells.default;
       };
--- build/makefile.nix
+++ build/makefile.nix
@@ -1,19 +1,67 @@
 {withSystem, ...}: let
   # Generate a wrapper Makefile handling the container activations.
-  # Given some 'hostPkgs' and a mapped 'system':
+  # Given some 'hostCtx' and a mapped 'mappedCtx':
   # - Grab the devshell package from the mapped system
   # - Create a makefile derivation using 'hostPkgs' but pointing to the mapped devshell.
-  mkMakefile = hostPkgs: {system ? hostPkgs.stdenv.system, ...}:
-    withSystem system ({self', ...}: let
-      inherit (self'.packages) devshell;
+  mkMakefile = hostCtx: {system ? hostCtx.pkgs.stdenv.system, ...}:
+    withSystem system (mappedCtx: let
+      devshell = hostCtx.pkgs.dockerTools.streamLayeredImage {
+        name = "devshell";
+
+        fromImage = hostCtx.pkgs.dockerTools.pullImage {
+          imageName = "ubuntu";
+          imageDigest = "sha256:83f0c2a8d6f266d687d55b5cb1cb2201148eb7ac449e4202d9646b9083f1cee0";
+          sha256 = "sha256-5y6ToMw1UGaLafjaN69YabkjyCX61FT3QxU4mtmXMP0=";
+          finalImageName = "ubuntu";
+          finalImageTag = "latest";
+        };
+
+        contents = let
+          mkEnv = pkgs:
+            pkgs.buildEnv {
+              name = "devshell-contents";
+              paths =
+                # seems to be mandatory
+                [pkgs.dockerTools.binSh pkgs.bashInteractive]
+                ++ (with pkgs;
+                  [
+                    just
+                  ]
+                  ++ [
+                    fd
+                    ripgrep
+                    zoxide
+                  ]
+                  ++ [
+                    bat
+                    (bat-extras.batdiff.override {withDelta = true;})
+                    bat-extras.batgrep
+                  ]
+                  ++ [glow]
+                  ++ [direnv]
+                  ++ [starship]
+                  ++ [coreutils]
+                  ++ [cowsay hello]);
+
+              pathsToLink = ["/bin" "/share"];
+            };
+        in
+          mkEnv mappedCtx.pkgs;
+
+        config = {
+          Env = [
+          ];
+        };
+      };
       image = "${devshell.imageName}:${devshell.imageTag}";
     in
-      hostPkgs.substituteAll rec {
+      hostCtx.pkgs.substituteAll rec {
         src = ./Makefile.in;
-        inherit devshell image;
+        inherit image;
+        loader = hostCtx.pkgs.lib.getExe devshell;
 
         # Generate a devcontainer.json with the 'image' parameter set to this flake's devshell.
-        template = hostPkgs.substituteAll {
+        template = hostCtx.pkgs.substituteAll {
           src = ./devcontainer.json.in;
           inherit image;
         };
@@ -29,9 +77,8 @@ in {
     };
   };
 
-  flake.packages.aarch64-darwin.makefile = withSystem "aarch64-darwin" ({pkgs, ...}: let
-    hostPkgs = pkgs;
-    makefile = mkMakefile hostPkgs {system = "aarch64-linux";};
+  flake.packages.aarch64-darwin.makefile = withSystem "aarch64-darwin" (hostCtx: let
+    makefile = mkMakefile hostCtx {system = "aarch64-linux";};
   in
     makefile);
 }
