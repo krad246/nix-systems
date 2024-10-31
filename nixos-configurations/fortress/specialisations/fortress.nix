f7624e7dea200d5090c577296000fb22308e28fe
Keerthi Radhakrishnan <krad246@gmail.com>
2024-10-30 21:24:09 -0400

    Enable s2idle on fortress

--- nixos-configurations/fortress/specialisations/fortress.nix
+++ nixos-configurations/fortress/specialisations/fortress.nix
@@ -65,7 +65,7 @@ in {
 
         boot = {
           binfmt.emulatedSystems = lib.lists.remove pkgs.stdenv.system ["aarch64-linux"];
-          kernelParams = ["usbcore.old_scheme_first=1"];
+          kernelParams = ["usbcore.old_scheme_first=1" "mem_sleep_default=s2idle"];
         };
 
         virtualisation.docker.enable = true;
