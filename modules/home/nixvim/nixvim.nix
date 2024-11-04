39f7bd5fd57b8576b01c9d4bf180c239b70d9114
Keerthi Radhakrishnan <krad246@gmail.com>
2024-11-04 13:09:30 -0500

    Correct nixvim module

--- modules/home/nixvim/nixvim.nix
+++ modules/home/nixvim/nixvim.nix
@@ -1,15 +1,21 @@
 {
   inputs,
   pkgs,
+  lib,
   ...
 }: let
   inherit (pkgs.stdenv) system;
+  nixvim = inputs.nixvim-config.packages.${system}.default;
 in {
-  programs.neovim = {
-    enable = true;
-    viAlias = true;
-    vimAlias = true;
-    defaultEditor = true;
-    package = inputs.nixvim-config.packages.${system}.default;
+  home = {
+    packages = [nixvim];
+    shellAliases = {
+      vi = lib.getExe nixvim;
+      vim = lib.getExe nixvim;
+      vimdiff = "${lib.getExe nixvim} -d";
+    };
+    sessionVariables = {
+      EDITOR = lib.getExe nixvim;
+    };
   };
 }
