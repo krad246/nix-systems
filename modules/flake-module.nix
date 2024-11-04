efeb955628f663c219ecbfe6fa6c3edc5a82efcf
Keerthi Radhakrishnan <krad246@gmail.com>
2024-11-04 10:58:33 -0500

    Centralize modules instantiation

--- modules/flake-module.nix
+++ modules/flake-module.nix
@@ -1,3 +1,21 @@
-_: {
-  imports = [./generic];
+_: {lib, ...}: let
+  mkModules = dir: let
+    findNix = path: lib.fileset.fileFilter (file: file.hasExt "nix") path;
+    hits = lib.fileset.toList (findNix dir);
+  in
+    lib.attrsets.mergeAttrsList (lib.lists.forEach hits (mname: let
+      bname = builtins.baseNameOf mname;
+      attrname = lib.strings.removeSuffix ".nix" bname;
+    in {
+      "${attrname}" = {
+        imports = [mname];
+      };
+    }));
+in {
+  flake.modules = {
+    generic = mkModules ./generic;
+    darwin = mkModules ./darwin;
+    nixos = mkModules ./nixos;
+    home = mkModules ./home;
+  };
 }
