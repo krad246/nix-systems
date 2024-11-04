259eb5fa96c16cb2aa88d9860be636d06a4f1987
Keerthi Radhakrishnan <krad246@gmail.com>
2024-11-04 10:42:41 -0500

    Correct ccache module

--- nixos-modules/ccache-stdenv.nix
+++ nixos-modules/ccache-stdenv.nix
@@ -1,5 +1,9 @@
-{config, ...}: {
-  imports = [../common/ccache-stdenv.nix];
+{
+  self,
+  config,
+  ...
+}: {
+  imports = [self.modules.generic.ccache-stdenv];
 
   systemd.tmpfiles.rules = [
     "d ${config.programs.ccache.cacheDir}                        770 root    nixbld  - -"
