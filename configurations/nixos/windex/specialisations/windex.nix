6ec0850fab6a7bc0ecc70b2154cc2e709a952c6f
Keerthi Radhakrishnan <krad246@gmail.com>
2024-11-04 15:57:53 -0500

    enable windex binfmt

--- configurations/nixos/windex/specialisations/windex.nix
+++ configurations/nixos/windex/specialisations/windex.nix
@@ -1,7 +1,13 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   specialisation = {
     ${config.networking.hostName} = {
       configuration = {
+        boot.binfmt.emulatedSystems = lib.lists.remove pkgs.stdenv.system ["aarch64-linux"];
       };
     };
   };
