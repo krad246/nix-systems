6703e320066097593d761ad6bb1331d77d92d60c
Keerthi Radhakrishnan <krad246@gmail.com>
2024-11-18 11:22:17 -0500

    drop devshell folder in packages; common fhsenv

--- build/packages/default.nix
+++ build/packages/default.nix
@@ -1,7 +1,39 @@
 {pkgs, ...}: let
   inherit (pkgs) lib;
 in {
-  packages = lib.attrsets.optionalAttrs pkgs.stdenv.isLinux {
-    disko-install = import ./disko-install.nix {inherit pkgs;};
-  };
+  packages =
+    (lib.attrsets.optionalAttrs pkgs.stdenv.isLinux {
+      disko-install = import ./disko-install.nix {inherit pkgs;};
+    })
+    // (let
+      targetPkgs = tpkgs:
+        with tpkgs;
+          [git]
+          ++ [direnv nix-direnv]
+          ++ [just gnumake]
+          ++ [shellcheck nil];
+
+      profile = ''
+        if [[ -f /.dockerenv ]]; then
+          # nix-build doesn't play very nice with the sticky bit
+          # and /tmp in a docker environment. unsetting it enables
+          # the container to manage its tmpfs as it pleases.
+          unset TEMP TMPDIR NIX_BUILD_TOP
+        else
+          :
+        fi
+      '';
+    in
+      (lib.attrsets.optionalAttrs pkgs.stdenv.isLinux {
+        devshell-bwrapenv = pkgs.buildFHSEnvBubblewrap {
+          name = "devshell-bwrapenv";
+          inherit targetPkgs profile;
+        };
+      })
+      // {
+        devshell-chrootenv = pkgs.buildFHSEnvChroot {
+          name = "devshell-chrootenv";
+          inherit targetPkgs profile;
+        };
+      });
 }
