841d7cd1581691eeb217c50ede52aa9684b271b4
Keerthi Radhakrishnan <krad246@gmail.com>
2024-11-18 12:37:09 -0500

    bwrapenv compatibility for direnv

--- build/devshell/default.nix
+++ build/devshell/default.nix
@@ -5,14 +5,42 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  inherit (pkgs) lib;
+in {
   devShells =
     {
-      default = import ./devshell.nix {inherit self' config pkgs;};
+      default = self'.devShells.nix-shell;
+      nix-shell = pkgs.mkShell {
+        inputsFrom = [
+          config.flake-root.devShell
+          config.just-flake.outputs.devShell
+          config.treefmt.build.devShell
+          config.pre-commit.devShell
+        ];
+
+        packages = with pkgs;
+          [git]
+          ++ [direnv nix-direnv]
+          ++ [just gnumake]
+          ++ [shellcheck nil]
+          ++ [devcontainer docker]
+          ++ [nix-prefetch-docker];
+
+        shellHook = ''
+          if [[ -f /.dockerenv ]]; then
+            # nix-build doesn't play very nice with the sticky bit
+            # and /tmp in a docker environment. unsetting it enables
+            # the container to manage its tmpfs as it pleases.
+            unset TEMP TMPDIR NIX_BUILD_TOP
+          else
+            :
+          fi
+        '';
+      };
     }
     // {
-      chrootenv = self'.packages.devshell-chrootenv.env;
-      bwrapenv = self'.packages.devshell-bwrapenv.env;
+      bwrapenv = lib.mkIf pkgs.stdenv.isLinux self'.packages.devshell-bwrapenv.env;
     };
 
   just-flake.features = import ./just-flake.nix {
--- build/packages/default.nix
+++ build/packages/default.nix
@@ -10,49 +10,55 @@ in {
     (lib.attrsets.optionalAttrs pkgs.stdenv.isLinux {
       disko-install = import ./disko-install.nix {inherit self pkgs;};
     })
-    // (let
-      # TODO: must port devShell inputsFrom.
-      inputsFrom = [
-        config.flake-root.devShell
-        config.just-flake.outputs.devShell
-        config.treefmt.build.devShell
-        config.pre-commit.devShell
-      ];
+    // (lib.attrsets.optionalAttrs pkgs.stdenv.isLinux {
+      devshell-bwrapenv = pkgs.buildFHSEnvBubblewrap {
+        name = "bwrapenv";
 
-      # 1. get all `{build,nativeBuild,...}Inputs` from the elements of `inputsFrom`
-      # 2. since that is a list of lists, `flatten` that into a regular list
-      # 3. filter out of the result everything that's in `inputsFrom` itself
-      # this leaves actual dependencies of the derivations in `inputsFrom`, but never the derivations themselves
-      mergeInputs = name: (lib.subtractLists inputsFrom (lib.flatten (lib.catAttrs name inputsFrom)));
+        # equivalent of mkShell inputsFrom
+        # append devShell dependencies to nativeBuildInputs
+        nativeBuildInputs = let
+          inputsFrom = [
+            config.flake-root.devShell
+            config.just-flake.outputs.devShell
+            config.treefmt.build.devShell
+            config.pre-commit.devShell
+          ];
 
-      nativeBuildInputs = mergeInputs "nativeBuildInputs";
+          # 1. get all `{build,nativeBuild,...}Inputs` from the elements of `inputs`
+          # 2. since that is a list of lists, `flatten` that into a regular list
+          # 3. filter out of the result everything that's in `inputsFrom` itself
+          # this leaves actual dependencies of the derivations in `inputsFrom`, but never the derivations themselves
+          mergeInputs = inputs: name: (lib.subtractLists inputs (lib.flatten (lib.catAttrs name inputs)));
+        in
+          mergeInputs inputsFrom "nativeBuildInputs";
 
-      targetPkgs = tpkgs:
-        with tpkgs;
-          [git]
-          ++ [direnv nix-direnv]
-          ++ [just gnumake]
-          ++ [shellcheck nil];
+        # Core package list for the host
+        targetPkgs = tpkgs:
+          with tpkgs;
+            [git]
+            ++ [direnv nix-direnv]
+            ++ [just gnumake]
+            ++ [shellcheck nil];
 
-      profile = ''
-        if [[ -f /.dockerenv ]]; then
-          # nix-build doesn't play very nice with the sticky bit
-          # and /tmp in a docker environment. unsetting it enables
-          # the container to manage its tmpfs as it pleases.
-          unset TEMP TMPDIR NIX_BUILD_TOP
-        else
-          :
-        fi
-      '';
-    in
-      lib.attrsets.optionalAttrs pkgs.stdenv.isLinux {
-        devshell-bwrapenv = pkgs.buildFHSEnvBubblewrap {
-          name = "devshell";
-          inherit targetPkgs profile nativeBuildInputs;
-        };
-        devshell-chrootenv = pkgs.buildFHSEnvChroot {
-          name = "devshell";
-          inherit targetPkgs profile;
-        };
-      });
+        # Initial bashrc, equivalent to shellHook on mkShell
+        profile = ''
+          if [[ -f /.dockerenv ]]; then
+            # nix-build doesn't play very nice with the sticky bit
+            # and /tmp in a docker environment. unsetting it enables
+            # the container to manage its tmpfs as it pleases.
+            unset TEMP TMPDIR NIX_BUILD_TOP
+          else
+            :
+          fi
+        '';
+
+        unshareUser = true;
+        unshareIpc = true;
+        unsharePid = true;
+        unshareNet = true;
+        unshareUts = true;
+        unshareCgroup = true;
+        privateTmp = true;
+      };
+    });
 }
