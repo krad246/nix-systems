87bada3c3357988fc6ec4aca0c027a46bd6ad252
Keerthi Radhakrishnan <krad246@gmail.com>
2024-11-19 14:55:06 -0500

    Consolidate devshells

--- build/packages/default.nix
+++ build/packages/default.nix
@@ -5,43 +5,33 @@
   ...
 }: let
   inherit (pkgs) lib;
-in {
-  packages = let
-    # equivalent of mkShell inputsFrom
-    # append devShell dependencies to nativeBuildInputs
-    nativeBuildInputs = let
-      inputsFrom = [
-        config.flake-root.devShell
-        config.just-flake.outputs.devShell
-        config.treefmt.build.devShell
-        config.pre-commit.devShell
-      ];
 
-      # 1. get all `{build,nativeBuild,...}Inputs` from the elements of `inputs`
-      # 2. since that is a list of lists, `flatten` that into a regular list
-      # 3. filter out of the result everything that's in `inputsFrom` itself
-      # this leaves actual dependencies of the derivations in `inputsFrom`, but never the derivations themselves
-      mergeInputs = inputs: name: (lib.subtractLists inputs (lib.flatten (lib.catAttrs name inputs)));
-    in
-      mergeInputs inputsFrom "nativeBuildInputs";
+  # Core package list for the host
+  targetPkgs = tpkgs:
+    with tpkgs;
+      [git]
+      ++ [direnv nix-direnv]
+      ++ [just gnumake]
+      ++ [shellcheck nil];
 
-    # Core package list for the host
-    targetPkgs = tpkgs:
-      with tpkgs;
-        [git]
-        ++ [direnv nix-direnv]
-        ++ [just gnumake]
-        ++ [shellcheck nil];
+  # equivalent of mkShell inputsFrom
+  # append devShell dependencies to nativeBuildInputs
+  nativeBuildInputs = let
+    # 1. get all `{build,nativeBuild,...}Inputs` from the elements of `inputs`
+    # 2. since that is a list of lists, `flatten` that into a regular list
+    # 3. filter out of the result everything that's in `inputsFrom` itself
+    # this leaves actual dependencies of the derivations in `inputsFrom`, but never the derivations themselves
+    mergeInputs = inputs: name: (lib.subtractLists inputs (lib.flatten (lib.catAttrs name inputs)));
   in
-    (lib.attrsets.optionalAttrs pkgs.stdenv.isLinux {
-      disko-install = import ./disko-install.nix {inherit self pkgs;};
-
-      devshell-bwrapenv = pkgs.buildFHSEnvBubblewrap {
-        name = "bwrapenv";
-        inherit nativeBuildInputs targetPkgs;
-      };
-    })
-    // {
+    mergeInputs [
+      config.flake-root.devShell
+      config.just-flake.outputs.devShell
+      config.treefmt.build.devShell
+      config.pre-commit.devShell
+    ] "nativeBuildInputs";
+in {
+  packages =
+    {
       nix-shell-env = let
         # construct a simple rootfs derivation with the packages
         paths = targetPkgs pkgs;
@@ -50,6 +40,8 @@ in {
           inherit paths;
         };
       in
+        # add a shellHook field to make it understandable by nix-shell
+        # pull in all the other dependencies from inputsFrom above
         fs.overrideAttrs (old: {
           nativeBuildInputs = old.nativeBuildInputs ++ nativeBuildInputs ++ paths;
           shellHook = ''
@@ -63,5 +55,17 @@ in {
             fi
           '';
         });
-    };
+    }
+    // (lib.attrsets.optionalAttrs pkgs.stdenv.isLinux {
+      # containerized / bwrap-ified devshell environment
+      devshell-bwrapenv = let
+        result = pkgs.buildFHSEnvBubblewrap {
+          name = "bwrapenv";
+          inherit nativeBuildInputs targetPkgs;
+        };
+      in
+        result;
+
+      disko-install = import ./disko-install.nix {inherit self pkgs;};
+    });
 }
