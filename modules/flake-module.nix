a5240031540794fbb5bf108a7f105a370ad85282
Keerthi Radhakrishnan <krad246@gmail.com>
2024-11-19 18:41:58 -0500

    docs

--- modules/flake-module.nix
+++ modules/flake-module.nix
@@ -4,13 +4,20 @@
   self,
   inputs,
   ...
-}: let
-  entrypoint = import ./flake {inherit withSystem importApply self inputs;};
-in
-  {lib, ...}: let
+}: {lib, ...}: {
+  # pull in the default flake module so that we can, y'know, build stuff...
+  imports = let
+    entrypoint = import ./flake {inherit withSystem importApply self inputs;};
+  in [entrypoint.flakeModule];
+
+  # scan subdirs for other modules
+  flake.modules = let
     mkModules = dir: let
+      # get a list of all .nix files
       findNix = path: lib.fileset.fileFilter (file: file.hasExt "nix") path;
       hits = lib.fileset.toList (findNix dir);
+      # for each nix file, create a key value pair of the form:
+      # { "${filename}" = import "${filename}"; }
     in
       lib.attrsets.mergeAttrsList (lib.lists.forEach hits (mname: let
         bname = builtins.baseNameOf mname;
@@ -21,12 +28,9 @@ in
         };
       }));
   in {
-    imports = [entrypoint.flakeModule];
-
-    flake.modules = {
-      generic = mkModules ./generic;
-      darwin = mkModules ./darwin;
-      nixos = mkModules ./nixos;
-      home = mkModules ./home;
-    };
-  }
+    generic = mkModules ./generic;
+    darwin = mkModules ./darwin;
+    nixos = mkModules ./nixos;
+    home = mkModules ./home;
+  };
+}
