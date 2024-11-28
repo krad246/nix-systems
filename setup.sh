41960049cef2f6a0ea91cbc16527b2cf396463ba
Keerthi Radhakrishnan <krad246@gmail.com>
2024-11-28 00:41:42 -0500

    shell script tweaks for shellcheck compliance

--- setup.sh
+++ setup.sh
@@ -3,5 +3,8 @@
 SCRIPT="$(realpath "$0")"
 SCRIPTPATH="$(dirname "$SCRIPT")"
 
+# shellcheck source=sh/install-nix.sh
 source "$SCRIPTPATH/sh/install-nix.sh"
+
+# shellcheck source=sh/bootstrap.sh
 exec "$SCRIPTPATH/sh/bootstrap.sh" "$@"
