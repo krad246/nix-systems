89dbd1d7c3836ad3649fb483ea8c6262ecab3339
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2024-12-05 21:43:02 -0500

    Delete nvim module

--- modules/home/shellenv/nvim/lua/plugins/comment.lua
+++
@@ -1,7 +0,0 @@
-return {
-    {
-        "numToStr/Comment.nvim",
-        opts = {},
-        event = { "BufReadPost", "BufNewFile" },
-    }
-}
--- modules/home/shellenv/nvim/lua/plugins/gitsigns.lua
+++
@@ -1,7 +0,0 @@
-return {
-    {
-        'lewis6991/gitsigns.nvim',
-        lazy = true,
-        opts = {}
-    }
-}
