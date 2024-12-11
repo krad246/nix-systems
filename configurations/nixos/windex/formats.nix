711f4f424b08b63f13b75e310749bdce97856887
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2024-12-11 15:59:39 -0500

    Correct windex tarball declaration

--- configurations/nixos/windex/formats.nix
+++ configurations/nixos/windex/formats.nix
@@ -1,13 +1,14 @@
 {
   inputs,
   config,
+  lib,
   ...
 }: let
   inherit (inputs) nixos-generators;
 in {
   imports = [nixos-generators.nixosModules.all-formats];
 
-  formats = {
+  formats = lib.modules.mkForce {
     tarball = config.system.build.tarballBuilder;
   };
 
