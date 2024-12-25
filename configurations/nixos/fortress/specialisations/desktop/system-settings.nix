65e27e514e9d9522947197e3cde6d7ce5ee27a3f
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2024-12-25 14:27:30 -0500

    Add a nerdfonts module; inhibit sleep

--- configurations/nixos/fortress/specialisations/desktop/system-settings.nix
+++ configurations/nixos/fortress/specialisations/desktop/system-settings.nix
@@ -37,18 +37,36 @@
               vscode-server
             ];
 
-            services = {
-              flatpak = {
-                packages = [
-                  "us.zoom.Zoom"
-                  "org.signal.Signal"
-                  "com.spotify.Client"
-                  "com.valvesoftware.Steam"
-                ];
-              };
-            };
+  home-manager.sharedModules = let
+    fortress = {
+      configuration = {...}: {
+        imports = with self.homeModules; [
+          discord
+          kdeconnect
+          vscode
+          vscode-server
+        ];
+
+        services = {
+          flatpak = {
+            packages = [
+              "us.zoom.Zoom"
+              "org.signal.Signal"
+              "com.spotify.Client"
+              "com.valvesoftware.Steam"
+            ];
           };
         };
+      };
+    };
+  in [
+    ({lib, ...}: {
+      imports = [
+        fortress.configuration
+      ];
+      specialisation = rec {
+        default = lib.modules.mkForce fortress;
+        inherit fortress;
       };
     })
   ];
