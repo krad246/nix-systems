0e7263292089a17aec12df40b6b46306c4c6535c
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2024-12-24 19:10:43 -0500

    Add spotlight module

--- modules/darwin/keyboard.nix
+++ modules/darwin/keyboard.nix
@@ -1,26 +1,32 @@
 {
-  system.defaults = {
-    # disable fn key bc it's annoying
-    hitoolbox.AppleFnUsageType = "Do Nothing";
+  system = {
+    defaults = {
+      # disable fn key bc it's annoying
+      hitoolbox.AppleFnUsageType = "Do Nothing";
 
-    NSGlobalDomain = {
-      # Enable full keyboard access for all controls
-      # (e.g. enable Tab in modal dialogs)
-      AppleKeyboardUIMode = 3;
+      NSGlobalDomain = {
+        # Enable full keyboard access for all controls
+        # (e.g. enable Tab in modal dialogs)
+        AppleKeyboardUIMode = 3;
 
-      NSAutomaticCapitalizationEnabled = true;
-      NSAutomaticDashSubstitutionEnabled = true;
-      NSAutomaticPeriodSubstitutionEnabled = true;
-      NSAutomaticQuoteSubstitutionEnabled = true;
-      NSAutomaticSpellingCorrectionEnabled = true;
-      NSAutomaticWindowAnimationsEnabled = true;
+        NSAutomaticCapitalizationEnabled = true;
+        NSAutomaticDashSubstitutionEnabled = true;
+        NSAutomaticPeriodSubstitutionEnabled = true;
+        NSAutomaticQuoteSubstitutionEnabled = true;
+        NSAutomaticSpellingCorrectionEnabled = true;
+        NSAutomaticWindowAnimationsEnabled = true;
 
-      # Set a blazingly fast keyboard repeat rate
-      InitialKeyRepeat = 12;
-      KeyRepeat = 3;
+        # Set a blazingly fast keyboard repeat rate
+        InitialKeyRepeat = 12;
+        KeyRepeat = 3;
 
-      # f1, f2 etc available
-      "com.apple.keyboard.fnState" = true;
+        # f1, f2 etc available
+        "com.apple.keyboard.fnState" = true;
+      };
+    };
+
+    keyboard = {
+      enableKeyMapping = true;
     };
   };
 }
