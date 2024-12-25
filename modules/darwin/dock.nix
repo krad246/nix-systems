5d3797f377b6b134b07a5d10c10135ced63a4fff
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2024-12-25 14:27:30 -0500

    Add a nerdfonts module; inhibit sleep

--- modules/darwin/dock.nix
+++ modules/darwin/dock.nix
@@ -42,9 +42,9 @@
       tilesize = 64;
 
       wvous-bl-corner = 11;
-      wvous-br-corner = 1;
-      wvous-tl-corner = 1;
-      wvous-tr-corner = 1;
+      wvous-br-corner = 4;
+      wvous-tl-corner = 2;
+      wvous-tr-corner = 12;
     };
 
     spaces.spans-displays = false;
