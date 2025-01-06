b5896d7f1136f7113d26a71d8b8229cd01dd99e5
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-01-05 22:28:46 -0500

    Introduce zen browser

--- modules/darwin/magnet.nix
+++ modules/darwin/magnet.nix
@@ -1,7 +1,19 @@
 {
-  homebrew = {
-    masApps = {
-      "Magnet" = 441258766;
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.krad246.darwin.apps.magnet;
+in {
+  options = {
+    krad246.darwin.apps.magnet = lib.options.mkEnableOption "magnet" // {default = true;};
+  };
+
+  config = {
+    homebrew = {
+      masApps = {
+        "Magnet" = lib.modules.mkIf cfg 441258766;
+      };
     };
   };
 }
--- modules/home/zen-browser.nix
+++
@@ -1,7 +0,0 @@
-{
-  withSystem,
-  pkgs,
-  ...
-}: {
-  home.packages = [(withSystem pkgs.stdenv.system ({inputs', ...}: inputs'.zen-browser.packages.default))];
-}
