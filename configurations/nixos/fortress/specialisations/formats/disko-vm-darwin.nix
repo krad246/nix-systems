abf40c67e230bb797c39c8abc99229d163e5024d
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-01-21 11:31:35 -0500

    Figure out how to expose the disko VM to darwin stdenvs

--- configurations/nixos/fortress/specialisations/formats/disko-vm-darwin.nix
+++ configurations/nixos/fortress/specialisations/formats/disko-vm-darwin.nix
@@ -1,7 +1,14 @@
-{inputs, ...}: {
+{
+  withSystem,
+  lib,
+  pkgs,
+  ...
+}: let
+  parse = lib.systems.parse.mkSystemFromString pkgs.stdenv.system;
+  arch = parse.cpu.name;
+in {
   formatAttr = "vmWithDisko";
   virtualisation.vmVariantWithDisko = {
-    virtualisation.host.pkgs = inputs.nixpkgs.legacyPackages.aarch64-darwin;
-    disko.memSize = 6 * 1024;
+    virtualisation.host.pkgs = withSystem "${arch}-darwin" ({pkgs, ...}: pkgs);
   };
 }
