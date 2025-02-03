e5b8210a7b84a17ca8533c23e9f7b4f43c8fcea7
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-02-03 16:07:36 -0500

    Make a machine module

---
+++ modules/darwin/machine/linux-builder.nix
@@ -0,0 +1,121 @@
+global @ {
+  self,
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.krad246.darwin.virtualisation.linux-builder;
+  inherit (lib) options types;
+in {
+  options = {
+    krad246.darwin.virtualisation.linux-builder = {
+      enable = (options.mkEnableOption "linux-builder") // {default = true;};
+
+      maxJobs = options.mkOption {
+        default = 16;
+        inherit (global.options.nix.linux-builder.maxJobs) type defaultText example description;
+      };
+
+      cores = options.mkOption {
+        type = types.ints.positive;
+        default = 8;
+        description = ''
+          Specify the number of cores the guest is permitted to use.
+          The number can be higher than the available cores on the
+          host system.
+        '';
+      };
+
+      memorySize = options.mkOption {
+        default = 6 * 1024;
+        type = types.ints.positive;
+        example = 8192;
+        description = "The runner's memory in MB";
+      };
+
+      diskSize = options.mkOption {
+        default = 32 * 1024;
+        type = types.ints.positive;
+        example = 30720;
+        description = "The maximum disk space allocated to the runner in MB";
+      };
+
+      swapSize = options.mkOption {
+        default = 16 * 1024;
+        type = types.ints.positive;
+        example = 30720;
+        description = "The maximum disk space allocated to the runner's swapfile in MB";
+      };
+
+      ephemeral = options.mkEnableOption "ephemeral";
+
+      extraConfig = options.mkOption {
+        inherit (global.options.nix.linux-builder.config) type default example description;
+      };
+
+      systems = options.mkOption {
+        default = ["i386-linux" "i686-linux" "x86_64-linux" "aarch64-linux"];
+        inherit (global.options.nix.linux-builder.systems) type description defaultText;
+      };
+    };
+  };
+
+  config = {
+    nix = {
+      settings.trusted-users = ["@admin" "@wheel"];
+
+      linux-builder = {
+        inherit (cfg) enable;
+        protocol = "ssh-ng";
+
+        config = {pkgs, ...}: let
+          inherit (pkgs.stdenv) system;
+          inherit (config.nix) linux-builder;
+        in {
+          imports =
+            (with self; [
+              modules.generic.nix-core
+              nixosModules.ccache-stdenv
+              nixosModules.zram
+            ])
+            ++ [cfg.extraConfig];
+
+          # Emulate all of the linux-builder systems
+          boot.binfmt.emulatedSystems = lib.lists.remove system linux-builder.systems;
+
+          virtualisation = {
+            inherit (cfg) cores;
+            darwin-builder = {
+              inherit (cfg) diskSize memorySize;
+            };
+          };
+
+          environment = {
+            systemPackages = with pkgs; [bottom];
+            variables = {
+              TERM = "xterm-256color";
+            };
+          };
+
+          # crashed processes aren't worth debugging in this environment tbh
+          systemd.coredump.enable = false;
+
+          # setting priority of swap devices to 1 less than mkVMOverride
+          # this makes it take precedence over the default behavior of no swap devices
+          # alternatively, you *could* reimplement everything via the defaultFileSystems argument
+          # but that stinks.
+          swapDevices = lib.mkOverride 9 [
+            {
+              device = "/swapfile";
+              size = cfg.swapSize;
+            }
+          ];
+        };
+
+        inherit (cfg) maxJobs;
+        inherit (cfg) ephemeral;
+        inherit (cfg) systems;
+      };
+    };
+  };
+}
---
+++ modules/darwin/machine/menubar.nix
@@ -0,0 +1,15 @@
+{
+  system.defaults = {
+    NSGlobalDomain = {
+      NSNavPanelExpandedStateForSaveMode = true;
+      NSNavPanelExpandedStateForSaveMode2 = true;
+
+      NSTableViewDefaultSizeMode = 2;
+
+      PMPrintingExpandedStateForPrint = true;
+      PMPrintingExpandedStateForPrint2 = true;
+
+      _HIHideMenuBar = false;
+    };
+  };
+}
