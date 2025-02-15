ab97b3046550a886381a97af83353690b2b0eb81
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-02-15 01:55:30 -0500

    Condense secrets

--- configurations/darwin/dullahan/hercules-ci/dullahan.nix
+++ configurations/darwin/dullahan/hercules-ci/dullahan.nix
@@ -1,11 +1,24 @@
-{specialArgs, ...}: {
+{
+  specialArgs,
+  config,
+  ...
+}: {
   services.hercules-ci-agent = {
     enable = true;
+    settings = {
+      binaryCachesPath = config.age.secrets.dullahan-binary-caches.path;
+      clusterJoinTokenPath = config.age.secrets.dullahan-cluster-join-token.path;
+    };
   };
 
   age.secrets = let
     inherit (specialArgs) krad246;
     paths = krad246.fileset.filterExt "age" ../secrets/system/hercules-ci;
   in
-    krad246.attrsets.genAttrs' paths (path: krad246.attrsets.stemValuePair path {file = path;});
+    krad246.attrsets.genAttrs' paths (path:
+      krad246.attrsets.stemValuePair path {
+        file = path;
+        mode = "770";
+        group = "hercules-ci-agent";
+      });
 }
