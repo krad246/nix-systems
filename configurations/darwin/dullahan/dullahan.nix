e22aa285d2cf590af7baebf8e3575f69eda77c06
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-02-20 19:12:03 -0500

    Disable a bunch  of brew apps on dullahan

--- configurations/darwin/dullahan/dullahan.nix
+++ configurations/darwin/dullahan/dullahan.nix
@@ -6,17 +6,30 @@
 }: {
   imports = [self.darwinModules.apps];
 
-  krad246.darwin.masterUser = {
-    enable = true;
-    owner = rec {
-      name = "krad246";
-      home = "/Users/${name}";
+  krad246.darwin = {
+    apps = {
+      arc = false;
+      bluesnooze = false;
+      groupme = false;
+      launchcontrol = false;
+      magnet = false;
+      signal = false;
+      utm = true;
+      zen-browser = false;
+      zoom = false;
+    };
+    masterUser = {
+      enable = true;
+      owner = rec {
+        name = "krad246";
+        home = "/Users/${name}";
 
-      uid = 501;
-      gid = 20;
+        uid = 501;
+        gid = 20;
 
-      shell = "${config.homebrew.brewPrefix}/bash";
-      createHome = true;
+        shell = "${config.homebrew.brewPrefix}/bash";
+        createHome = true;
+      };
     };
   };
 
