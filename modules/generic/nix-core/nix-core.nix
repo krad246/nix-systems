464927bc8ad5e264778312bf324c4b39a2d2e54a
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-02-24 11:35:34 -0500

    Refactor the nix-core module to force enable the sandbox.

--- modules/generic/nix-core/nix-core.nix
+++ modules/generic/nix-core/nix-core.nix
@@ -10,14 +10,21 @@ in {
     checkConfig = true;
     gc.automatic = true;
     settings = {
-      use-xdg-base-directories = true;
-      builders-use-substitutes = true;
       experimental-features = ["nix-command" "flakes"];
+      preallocate-contents = true;
+      use-xdg-base-directories = true;
+
+      keep-going = true;
+      max-jobs = "auto";
+      max-substitution-jobs = modules.mkDefault 16;
+
+      auto-optimise-store = modules.mkDefault false;
+      min-free = modules.mkDefault (12 * 1024 * 1024 * 1024);
+
       keep-outputs = modules.mkDefault true;
       keep-derivations = modules.mkDefault false;
-      auto-optimise-store = modules.mkDefault false;
-      sandbox = modules.mkIf pkgs.stdenv.isLinux "relaxed";
-      sandbox-fallback = modules.mkForce pkgs.stdenv.isDarwin;
+
+      builders-use-substitutes = true;
       substituters = [
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
@@ -33,14 +40,13 @@ in {
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "krad246.cachix.org-1:N57J9SfNFtxMSYnlULH4l7ZkdNjIQb0ByyapaEb/8IM="
       ];
+
       connect-timeout = modules.mkDefault 300;
       timeout = modules.mkDefault 1800;
       max-silent-time = modules.mkDefault 3600;
-      keep-going = true;
-      min-free = modules.mkDefault 12884901888;
-      preallocate-contents = true;
-      max-jobs = modules.mkDefault "auto";
-      max-substitution-jobs = modules.mkDefault 16;
+
+      sandbox = true;
+      sandbox-fallback = true;
     };
   };
 }
