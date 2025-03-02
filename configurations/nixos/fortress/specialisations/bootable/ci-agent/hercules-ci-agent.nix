af45220f989d0becaa665d6739e3501c2bb71593
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-03-01 21:16:39 -0500

    Change nix daemon log level

--- configurations/nixos/fortress/specialisations/bootable/ci-agent/hercules-ci-agent.nix
+++ configurations/nixos/fortress/specialisations/bootable/ci-agent/hercules-ci-agent.nix
@@ -5,6 +5,7 @@
       clusterJoinTokenPath = config.age.secrets.cluster-join-token.path;
       binaryCachesPath = config.age.secrets.binary-caches.path;
       concurrentTasks = 48;
+      nixVerbosity = "Warn";
     };
   };
 }
