6166384aa931274caa6252121be3a4d5eb879392
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-03-19 10:21:41 -0400

    Hardcode packages to build

--- flakeModules/hercules-ci/flake-module.nix
+++ flakeModules/hercules-ci/flake-module.nix
@@ -1,5 +1,6 @@
 {
   inputs,
+  withSystem,
   self,
   lib,
   ...
@@ -36,23 +37,21 @@
       getTopLevelDrv = cfg: cfg.config.system.build.toplevel;
       getDrvs = cfgs: lib.attrsets.mapAttrs (_name: getTopLevelDrv) cfgs;
     in rec {
+      checks.outputs = self.checks;
+      darwinConfigurations.outputs = getDrvs self.darwinConfigurations;
+      devShells.outputs = self.devShells;
+      nixosConfigurations.outputs = getDrvs self.nixosConfigurations;
       default.outputs = lib.modules.mkForce {
         checks = checks.outputs;
         darwinConfigurations = darwinConfigurations.outputs;
         devShells = devShells.outputs;
         nixosConfigurations = nixosConfigurations.outputs;
-        packages = lib.attrsets.mapAttrs (_system: packages:
-          lib.attrsets.intersectAttrs packages {
-            fortress-disko-vm = 1;
-            windex-tarball = 1;
-          })
-        self.packages;
+        packages = {
+          aarch64-darwin = withSystem "aarch64-darwin" ({self', ...}: {
+            inherit (self'.packages) fortress-disko-vm;
+          });
+        };
       };
-
-      checks.outputs = self.checks;
-      darwinConfigurations.outputs = getDrvs self.darwinConfigurations;
-      devShells.outputs = self.devShells;
-      nixosConfigurations.outputs = getDrvs self.nixosConfigurations;
     };
   };
 }
