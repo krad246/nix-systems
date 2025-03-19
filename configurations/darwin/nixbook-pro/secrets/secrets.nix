3ba6c632629c9469d34fdf0c8566dd14ca94a07f
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-03-19 11:17:46 -0400

    Revoke stale pubkeys from secrets.nix and delete unreferenced secrets

--- configurations/darwin/nixbook-pro/secrets/secrets.nix
+++ configurations/darwin/nixbook-pro/secrets/secrets.nix
@@ -1,11 +1,9 @@
 let
   nixbook-pro = {
     krad246 = {
-      ed25519 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINnXYLXHf5YatmbglLujnTScz1xe7rjPRWXRpGydx4Hb condor-janitor0e@icloud.com";
     };
 
     system = {
-      ed25519 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIiMqE0sNF1dbKR1M4N2vca4pv0k2Tp3l04Y3LHO6PHu root@nixbook-pro";
     };
   };
 
