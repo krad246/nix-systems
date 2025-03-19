fd32a16fcea155de33e3a74077848ccc04cfce9c
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-03-19 13:04:14 -0400

    Delete other exposed key details

--- configurations/darwin/dullahan/secrets/system/secrets.nix
+++ configurations/darwin/dullahan/secrets/system/secrets.nix
@@ -5,14 +5,13 @@ let
     };
 
     linux-builder = {
-      ed25519 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJBWcxb/Blaqt1auOtE+F8QUWrUotiC5qBJ+UuEWdVCb root@nixos";
     };
   };
 
   inherit (dullahan) system linux-builder;
 in {
-  "./hercules-ci/headless-penguin/headless-penguin-binary-caches.age".publicKeys = [system.ed25519 linux-builder.ed25519];
-  "./hercules-ci/headless-penguin/headless-penguin-cluster-join-token.age".publicKeys = [system.ed25519 linux-builder.ed25519];
+  "./hercules-ci/headless-penguin/headless-penguin-binary-caches.age".publicKeys = [system.ed25519];
+  "./hercules-ci/headless-penguin/headless-penguin-cluster-join-token.age".publicKeys = [system.ed25519];
 
   "./hercules-ci/dullahan-binary-caches.age".publicKeys = [system.ed25519];
   "./hercules-ci/dullahan-cluster-join-token.age".publicKeys = [system.ed25519];
