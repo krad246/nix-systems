042d3c94366424e5a4814608bb7a2b45408b0311
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-03-18 21:13:01 -0400

    Deprecate old modules

--- configurations/darwin/dullahan/hercules-ci/hercules-ci.nix
+++ configurations/darwin/dullahan/hercules-ci/hercules-ci.nix
@@ -1,9 +1,21 @@
 {
   self,
   specialArgs,
+  config,
   lib,
   ...
 }: {
+  services.hercules-ci-agent = {
+    enable = true;
+    settings = {
+      binaryCachesPath = config.age.secrets.dullahan-binary-caches.path;
+      clusterJoinTokenPath = config.age.secrets.dullahan-cluster-join-token.path;
+      concurrentTasks = 12;
+    };
+  };
+
+  environment.variables.NIX_REMOTE = "daemon";
+
   krad246.darwin.virtualisation.linux-builder.extraConfig = {config, ...}: {
     imports = [self.nixosModules.agenix];
 
