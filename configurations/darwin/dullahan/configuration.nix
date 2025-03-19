9674c31d43830948484a26fbe0f9ffc37b0777e8
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-03-19 05:33:11 -0400

    Syntax error

--- configurations/darwin/dullahan/configuration.nix
+++ configurations/darwin/dullahan/configuration.nix
@@ -26,43 +26,45 @@
   ids.gids.nixbld = 30000;
 
   # my custom overrides over the base-configuration module.
-  krad246.darwin = {
-    apps = {
-      arc = false;
-      bluesnooze = false;
-      groupme = false;
-      launchcontrol = false;
-      magnet = false;
-      signal = false;
-      utm = true;
-      zen-browser = false;
-      zoom = false;
-    };
-
-    masterUser = {
-      enable = true;
-      owner = rec {
-        name = "krad246";
-        home = "/Users/${name}";
-
-        uid = 501;
-        gid = 20;
-
-        shell = "${config.homebrew.brewPrefix}/bash";
-        createHome = true;
+  krad246 = {
+    darwin = {
+      apps = {
+        arc = false;
+        bluesnooze = false;
+        groupme = false;
+        launchcontrol = false;
+        magnet = false;
+        signal = false;
+        utm = true;
+        zen-browser = false;
+        zoom = false;
       };
-    };
 
-    virtualisation.linux-builder = {
-      ephemeral = false;
+      masterUser = {
+        enable = true;
+        owner = rec {
+          name = "krad246";
+          home = "/Users/${name}";
 
-      memorySize = 8 * 1024;
-      diskSize = 128 * 1024;
+          uid = 501;
+          gid = 20;
 
-      maxJobs = 32;
-      cores = 8;
+          shell = "${config.homebrew.brewPrefix}/bash";
+          createHome = true;
+        };
+      };
 
-      systems = ["aarch64-linux" "x86_64-linux"];
+      virtualisation.linux-builder = {
+        ephemeral = false;
+
+        memorySize = 8 * 1024;
+        diskSize = 128 * 1024;
+
+        maxJobs = 32;
+        cores = 8;
+
+        systems = ["aarch64-linux" "x86_64-linux"];
+      };
     };
 
     remotes.fortress = {
