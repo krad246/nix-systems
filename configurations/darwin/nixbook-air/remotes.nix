bab8ff866063a407d3401e736f8defeabbc92486
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-03-19 08:55:36 -0400

    Delete all authorized_keys references

--- configurations/darwin/nixbook-air/remotes.nix
+++ configurations/darwin/nixbook-air/remotes.nix
@@ -1,8 +1,4 @@
-{
-  self,
-  config,
-  ...
-}: {
+{self, ...}: {
   imports = [
     self.modules.generic.dullahan
     self.modules.generic.fortress
@@ -11,14 +7,12 @@
   krad246.remotes.dullahan = {
     enable = false;
     sshUser = "krad246";
-    sshKey = config.age.secrets.id_ed25519_priv.path;
     maxJobs = 24;
   };
 
   krad246.remotes.fortress = {
     enable = false;
     sshUser = "krad246";
-    sshKey = config.age.secrets.id_ed25519_priv.path;
     maxJobs = 144;
     speedFactor = 4;
   };
--- configurations/darwin/nixbook-pro/remotes.nix
+++ configurations/darwin/nixbook-pro/remotes.nix
@@ -1,8 +1,4 @@
-{
-  self,
-  config,
-  ...
-}: {
+{self, ...}: {
   imports = [
     self.modules.generic.dullahan
     self.modules.generic.fortress
@@ -12,14 +8,12 @@
     dullahan = {
       enable = true;
       sshUser = "krad246";
-      sshKey = config.age.secrets.id_ed25519_priv.path;
       maxJobs = 24;
     };
 
     fortress = {
       enable = true;
       sshUser = "krad246";
-      sshKey = config.age.secrets.id_ed25519_priv.path;
       maxJobs = 144;
       speedFactor = 2;
     };
