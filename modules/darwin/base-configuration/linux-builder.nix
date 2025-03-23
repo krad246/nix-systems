da9a823275fdd77d28130a5eea942e2e22e0fb4b
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-03-22 23:13:07 -0400

    Patch disappeared

--- modules/darwin/base-configuration/linux-builder.nix
+++ modules/darwin/base-configuration/linux-builder.nix
@@ -88,6 +88,19 @@ in {
             darwin-builder = {
               inherit (cfg) diskSize memorySize;
             };
+            qemu.package = pkgs.qemu.overrideAttrs (
+              _finalAttrs: previousAttrs: {
+                patches =
+                  previousAttrs.patches
+                  ++ pkgs.lib.optional pkgs.stdenv.targetPlatform.isDarwin (
+                    pkgs.fetchpatch {
+                      name = "fix-sme-darwin.patch";
+                      url = "https://github.com/utmapp/UTM/raw/acbf2ba8cd91f382a5e163c49459406af0b462b7/patches/qemu-9.1.0-utm.patch";
+                      hash = "sha256-S7DJSFD7EAzNxyQvePAo5ZZyanFrwQqQ6f2/hJkTJGA=";
+                    }
+                  );
+              }
+            );
           };
 
           environment = {
