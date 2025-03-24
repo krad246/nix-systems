49f5ee07075aecbb95a31a3a70f60abdeb81096c
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-03-24 17:14:43 -0400

    Remove privileged setting

--- configurations/nixos/fortress/configuration.nix
+++ configurations/nixos/fortress/configuration.nix
@@ -42,6 +42,4 @@
       ];
     }
   ];
-
-  nix.settings.trusted-users = ["krad246"];
 }
--- modules/nixos/base-configuration/default-users.nix
+++ modules/nixos/base-configuration/default-users.nix
@@ -11,4 +11,9 @@
   };
 
   security.sudo.wheelNeedsPassword = true;
+
+  # members of the wheel / admin group are able to execute privileged operations on the daemon.
+  # trusted users are able to specify arbitrary binary substitutes, etc. as well as signing custom outputs.
+  # otherwise, these outputs are not usable, and the daemon refuses to execute most 'advanced' operations.
+  nix.settings.trusted-users = ["@wheel"];
 }
