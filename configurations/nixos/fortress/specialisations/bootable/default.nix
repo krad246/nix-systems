75920dc5c2bce9be9f069f6811069909758ede7c
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-03-26 13:53:25 -0400

    Declutter fortress specialization code

--- configurations/nixos/fortress/specialisations/bootable/default.nix
+++ configurations/nixos/fortress/specialisations/bootable/default.nix
@@ -1,24 +1,22 @@
-let
-  shared = {self, ...}: {
+{
+  # Gaming desktop configuration
+  desktop.configuration = _: {
     imports =
-      [
-        ./avahi.nix
-        ./efiboot.nix
-        ./hardware-configuration.nix
-        ./sshd.nix
-      ]
+      [./base-configuration.nix]
       ++ [
-        self.diskoConfigurations.fortress-desktop
-        self.nixosModules.base-configuration
+        ./desktop/configuration.nix
       ];
+  };
 
-    services.tailscale = {
-      enable = true;
-      extraUpFlags = ["--ssh"];
-    };
-
-    # Holds up boot pointlessly
-    systemd.services.NetworkManager-wait-online.enable = false;
+  # always-on CI agent configuration
+  ci-agent.configuration = {...}: {
+    imports =
+      [./base-configuration.nix]
+      ++ [
+        ./ci-agent/agenix.nix
+        ./ci-agent/cachix-agent.nix
+        ./ci-agent/hercules-ci-agent.nix
+      ];
 
     # Prefer to idle in LPM only when explicitly requested
     systemd.sleep.extraConfig = ''
@@ -27,47 +25,13 @@ let
       AllowHybridSleep=yes
       AllowSuspendThenHibernate=yes
     '';
-  };
-in rec {
-  fortress = desktop;
-
-  desktop.configuration = _: {
-    imports =
-      [shared]
-      ++ [
-        ./desktop/configuration.nix
-      ];
-  };
-
-  ci-agent.configuration = {lib, ...}: {
-    imports =
-      [shared]
-      ++ [
-        ./ci-agent/agenix.nix
-        ./ci-agent/cachix-agent.nix
-        ./ci-agent/hercules-ci-agent.nix
-      ];
 
     nix = {
       settings.max-substitution-jobs = 144;
-      daemonIOSchedClass = lib.mkDefault "idle";
-      daemonCPUSchedPolicy = lib.mkDefault "idle";
     };
 
-    # put the service in top-level slice
-    # so that it's lower than system and user slice overall
-    # instead of only bing lower in system slice
-    systemd.services.nix-daemon.serviceConfig.Slice = "-.slice";
-
-    # always use the daemon, even executed  with root
+    # run in multi-user mode always
     environment.variables.NIX_REMOTE = "daemon";
-
-    virtualisation = {
-      docker.enable = true;
-      containerd.enable = true;
-    };
-
-    users.users.krad246.extraGroups = ["docker"];
 
     # large tmpfs for CI builds
     boot.tmp.tmpfsSize = "176G";
