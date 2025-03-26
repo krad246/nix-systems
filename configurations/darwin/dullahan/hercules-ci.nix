ae80cc8e9bcb9a99de3d995cfe1a5257c3c9f9b7
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-03-26 18:43:57 -0400

    Implement cross linkages between CI agents

--- configurations/darwin/dullahan/hercules-ci.nix
+++ configurations/darwin/dullahan/hercules-ci.nix
@@ -66,6 +66,8 @@
 
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIj/uRwLu2OnR99uZSIZYOVeLJGh1DLBrHRabYF2ofho _hercules-ci-agent@dullahan"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGD6hLeL5EzA8msaTLS964+fPIwtHPWSRZU3nVEGaNi5 _hercules-ci-agent@gremlin"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID/5yaElFDoFQtyZAg2yJaqr+7JjJx0LiWlRUoTRYkPL hercules-ci-agent@fortress"
   ];
 
   nix.gc.user = "root";
