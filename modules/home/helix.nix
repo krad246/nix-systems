15a3aa39e5fbee7519b08c716f7800a3cbcf7818
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-04-05 14:22:26 -0400

    Reorganize config, add some reimplementations of expected vim behaviors

--- modules/home/helix.nix
+++ modules/home/helix.nix
@@ -5,11 +5,50 @@
 }: {
   programs.helix = {
     enable = true;
-    package = pkgs.evil-helix;
     defaultEditor = true;
 
-    extraPackages = [
-    ];
+    package = pkgs.evil-helix;
+    extraPackages = [];
+
+    settings = {
+      theme = "gruvbox_dark_soft";
+
+      editor = {
+        evil = true;
+
+        cursor-shape = {
+          normal = "block";
+          insert = "bar";
+        };
+      };
+
+      keys = {
+        normal = {
+          s = "no_op"; # disable select_regex
+          "C-s" = "no_op"; # disable save_selection
+
+          # swap paste operations
+          # default behavior is to now paste at the cursor position
+
+          p = "paste_before";
+          P = "paste_after";
+
+          "0" = "goto_line_start";
+          "^" = "goto_first_nonwhitespace";
+          "$" = "goto_line_end";
+
+          G = "goto_file_end";
+          V = [
+            # "select_mode"
+            "extend_to_line_bounds"
+          ];
+          "C-a" = [
+            # "select_mode"
+            "select_all"
+          ];
+        };
+      };
+    };
 
     ignores = [];
 
@@ -42,27 +81,6 @@
       language-server = {
         marksman.command = lib.meta.getExe pkgs.marksman;
         nixd.command = lib.meta.getExe pkgs.nixd;
-      };
-    };
-
-    settings = {
-      theme = "gruvbox_dark_soft";
-      editor = {
-        evil = true;
-
-        cursor-shape = {
-          normal = "block";
-          insert = "bar";
-        };
-      };
-
-      keys = {
-        normal = {
-          V = [
-            "select_mode"
-            "extend_to_line_bounds"
-          ];
-        };
       };
     };
   };
