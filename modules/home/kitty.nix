28190cb7bf197178c0cc17a9663897831c749edb
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-04-06 14:56:06 -0400

    change tiling keybinds for kitty

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -21,6 +21,12 @@
       "ctrl+shift+e" = ""; # was 'open link'
       "ctrl+shift+r" = ""; # was 'window resize'
       "ctrl+shift+g" = ""; # was 'show last command output'
+
+      # a window is a tile in a 'tab', essentially
+      "ctrl+shift+]" = ""; # was 'next_window'
+      "alt+]" = "next_window";
+      "ctrl+shift+[" = "";
+      "alt+[" = "prev_window";
     };
     environment = {};
     extraConfig = ''
