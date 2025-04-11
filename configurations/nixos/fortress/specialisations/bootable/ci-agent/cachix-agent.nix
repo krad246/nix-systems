b8ea9365068c85281f34e4cbaf7841060761f182
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-04-11 18:58:18 -0400

    Clean up fortress CI agent secrets

--- configurations/nixos/fortress/specialisations/bootable/ci-agent/cachix-agent.nix
+++ configurations/nixos/fortress/specialisations/bootable/ci-agent/cachix-agent.nix
@@ -2,7 +2,12 @@
   services.cachix-watch-store = {
     enable = true;
     cacheName = "krad246";
-    cachixTokenFile = config.age.secrets.cachix.path;
+    cachixTokenFile = config.age.secrets.cachix-token.path;
     verbose = true;
+  };
+  age.secrets = {
+    cachix-token = {
+      file = ./secrets/cachix.age;
+    };
   };
 }
