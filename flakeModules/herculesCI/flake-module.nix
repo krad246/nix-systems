94e3bdf926f61b59877bd7f86053d71c33b37330
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-04-11 21:11:14 -0400

    Revert "Reorganize the effects a bit"
    This reverts commit e491c67915c9c0ba28c07704f68281921f31db0b.

--- flakeModules/herculesCI/flake-module.nix
+++ flakeModules/herculesCI/flake-module.nix
@@ -33,102 +33,138 @@
   };
 
   flake.effects = {
-    herculesCI = _herculesCI: {
-      onPush = rec {
-        checks.outputs = self.checks;
-        darwinConfigurations.outputs = lib.attrsets.mapAttrs (_name: value: lib.attrsets.dontRecurseIntoAttrs value.system) self.darwinConfigurations;
-        devShells.outputs = self.devShells;
-        nixosConfigurations.outputs = lib.attrsets.mapAttrs (_name: value: lib.attrsets.dontRecurseIntoAttrs value.config.system.build.toplevel) self.nixosConfigurations;
-        default.outputs = lib.modules.mkForce {
-          checks = checks.outputs;
-          darwinConfigurations = darwinConfigurations.outputs;
-          devShells = devShells.outputs;
-          nixosConfigurations = nixosConfigurations.outputs;
+    dullahan-deploy = withSystem "x86_64-linux" ({hci-effects, ...}:
+      hci-effects.runNixDarwin {
+        ssh = {
+          destination = "root@dullahan.tailb53085.ts.net";
+          destinationPkgs = withSystem "aarch64-darwin" (ctx: ctx.pkgs);
+          sshOptions = "-oSetEnv=PATH=/nix/var/nix/profiles/default/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin";
+        };
+        secretsMap.ssh = "default-ssh";
+        userSetupScript = ''
+          writeSSHKey
+
+          cat >>~/.ssh/known_hosts <<EOF
+          dullahan.tailb53085.ts.net ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJXafRdLT+qPTMUzzMc35PxOP4zun6zIPTf98jQ6Bv5P
+          EOF
+        '';
+
+        configuration = self.darwinConfigurations.dullahan;
+        buildOnDestination = true;
+      });
+
+    gremlin-deploy = withSystem "x86_64-linux" ({hci-effects, ...}:
+      hci-effects.runNixDarwin {
+        ssh = {
+          destination = "root@gremlin.tailb53085.ts.net";
+          destinationPkgs = withSystem "aarch64-darwin" (ctx: ctx.pkgs);
+          sshOptions = "-oSetEnv=PATH=/nix/var/nix/profiles/default/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin";
+        };
+
+        secretsMap.ssh = "default-ssh";
+        userSetupScript = ''
+          writeSSHKey
+
+          cat >>~/.ssh/known_hosts <<EOF
+          gremlin.tailb53085.ts.net ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL0IJSgLQ/JomKuYZVV5/ZuboysqBJQCgBcHTvKklQDb
+          EOF
+        '';
+
+        configuration = self.darwinConfigurations.gremlin;
+        buildOnDestination = true;
+      });
+
+    fortress-deploy = withSystem "x86_64-linux" ({
+      hci-effects,
+      pkgs,
+      ...
+    }:
+      hci-effects.runNixOS {
+        ssh = {
+          destination = "root@fortress.tailb53085.ts.net";
+          destinationPkgs = pkgs;
+          sshOptions = ""; # TODO: convert this to a userSetupScript line for strict host verification
+          # buildOnDestination = true;
+        };
+
+        secretsMap.ssh = "default-ssh";
+        userSetupScript = ''
+          writeSSHKey
+
+
+          cat >>~/.ssh/known_hosts <<EOF
+          fortress.tailb53085.ts.net ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDg9s+XAwWrvRf0k16ua9/3tpxbohrTGJEp4rOPnqgOh root@fortress
+          EOF
+        '';
+
+        config = self.nixosConfigurations.fortress.config.specialisation.ci-agent.configuration;
+        system = "x86_64-linux";
+      });
+  };
+
+  herculesCI = _herculesCI: {
+    onSchedule = {
+      dullahan-deploy = {
+        outputs.effects = {
+          inherit (self.effects) dullahan-deploy;
         };
       };
 
-      onSchedule = {
-        dullahan = {
-          outputs.effects = {
-            deploy = withSystem "x86_64-linux" ({hci-effects, ...}:
-              hci-effects.runNixDarwin {
-                ssh = {
-                  destination = "root@dullahan.tailb53085.ts.net";
-                  destinationPkgs = withSystem "aarch64-darwin" (ctx: ctx.pkgs);
-                  sshOptions = "-oSetEnv=PATH=/nix/var/nix/profiles/default/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin";
-                };
-                secretsMap.ssh = "default-ssh";
-                userSetupScript = ''
-                  writeSSHKey
+      gremlin-deploy = {
+        outputs.effects = {
+          inherit (self.effects) gremlin-deploy;
+        };
+      };
 
-                  cat >>~/.ssh/known_hosts <<EOF
-                  dullahan.tailb53085.ts.net ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJXafRdLT+qPTMUzzMc35PxOP4zun6zIPTf98jQ6Bv5P
-                  EOF
-                '';
+      fortress-deploy = {
+        outputs.effects = {
+          inherit (self.effects) fortress-deploy;
+        };
+      };
 
-                configuration = self.darwinConfigurations.dullahan;
-                buildOnDestination = true;
-              });
+      fortress-disko-vm = {
+        outputs = {
+          packages = {
+            aarch64-darwin = withSystem "aarch64-darwin" ({self', ...}: {
+              inherit (self'.packages) fortress-disko-vm;
+            });
+
+            aarch64-linux = withSystem "aarch64-linux" ({self', ...}: {
+              inherit (self'.packages) fortress-disko-vm;
+            });
+
+            x86_64-linux = withSystem "x86_64-linux" ({self', ...}: {
+              inherit (self'.packages) fortress-disko-vm;
+            });
           };
         };
+      };
 
-        gremlin = {
-          outputs.effects = {
-            deploy = withSystem "x86_64-linux" ({hci-effects, ...}:
-              hci-effects.runNixDarwin {
-                ssh = {
-                  destination = "root@gremlin.tailb53085.ts.net";
-                  destinationPkgs = withSystem "aarch64-darwin" (ctx: ctx.pkgs);
-                  sshOptions = "-oSetEnv=PATH=/nix/var/nix/profiles/default/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin";
-                };
+      windex-tarball = {
+        outputs = {
+          packages = {
+            aarch64-linux = withSystem "aarch64-linux" ({self', ...}: {
+              inherit (self'.packages) windex-tarball;
+            });
 
-                secretsMap.ssh = "default-ssh";
-                userSetupScript = ''
-                  writeSSHKey
-
-                  cat >>~/.ssh/known_hosts <<EOF
-                  gremlin.tailb53085.ts.net ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL0IJSgLQ/JomKuYZVV5/ZuboysqBJQCgBcHTvKklQDb
-                  EOF
-                '';
-
-                configuration = self.darwinConfigurations.gremlin;
-                buildOnDestination = true;
-              });
+            x86_64-linux = withSystem "x86_64-linux" ({self', ...}: {
+              inherit (self'.packages) windex-tarball;
+            });
           };
         };
+      };
+    };
 
-        fortress = {
-          outputs = {
-            effects = {
-              deploy = withSystem "x86_64-linux" ({
-                hci-effects,
-                pkgs,
-                ...
-              }:
-                hci-effects.runNixOS {
-                  ssh = {
-                    destination = "root@fortress.tailb53085.ts.net";
-                    destinationPkgs = pkgs;
-                    sshOptions = ""; # TODO: convert this to a userSetupScript line for strict host verification
-                    # buildOnDestination = true;
-                  };
-
-                  secretsMap.ssh = "default-ssh";
-                  userSetupScript = ''
-                    writeSSHKey
-
-
-                    cat >>~/.ssh/known_hosts <<EOF
-                    fortress.tailb53085.ts.net ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDg9s+XAwWrvRf0k16ua9/3tpxbohrTGJEp4rOPnqgOh root@fortress
-                    EOF
-                  '';
-
-                  config = self.nixosConfigurations.fortress.config.specialisation.ci-agent.configuration;
-                  system = "x86_64-linux";
-                });
-            };
-          };
-        };
+    onPush = rec {
+      checks.outputs = self.checks;
+      darwinConfigurations.outputs = lib.attrsets.mapAttrs (_name: value: lib.attrsets.dontRecurseIntoAttrs value.system) self.darwinConfigurations;
+      devShells.outputs = self.devShells;
+      nixosConfigurations.outputs = lib.attrsets.mapAttrs (_name: value: lib.attrsets.dontRecurseIntoAttrs value.config.system.build.toplevel) self.nixosConfigurations;
+      default.outputs = lib.modules.mkForce {
+        checks = checks.outputs;
+        darwinConfigurations = darwinConfigurations.outputs;
+        devShells = devShells.outputs;
+        nixosConfigurations = nixosConfigurations.outputs;
       };
     };
   };
