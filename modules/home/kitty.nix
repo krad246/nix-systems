7c2d8204855ef146aa30cb54969673baf72403f0
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-04-15 22:16:32 -0400

    Add some new kitty keybinds

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -20,7 +20,13 @@
     keybindings = {
       "ctrl+shift+e" = ""; # was 'open link'
       "ctrl+shift+r" = ""; # was 'window resize'
+
       "ctrl+shift+g" = ""; # was 'show last command output'
+      "ctrl+shift+up" = "scroll_to_prompt -1"; # was 'scroll to previous prompt'
+      "ctrl+shift+down" = "scroll_to_prompt 1"; # was 'scroll to next prompt'
+
+      "ctrl+shift+l" = ""; # was 'next layout'
+      "ctrl+l" = "clear_terminal to_cursor_scroll active";
 
       # a window is a tile in a 'tab', essentially
       "ctrl+shift+]" = ""; # was 'next_window'
