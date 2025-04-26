c2c0433558a918467c27969fd54009940f8bc6ac
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-04-26 07:58:33 -0400

    Reenable some keybinds

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -23,37 +23,46 @@
 
         "kitty_mod+c" = "copy_to_clipboard";
         "cmd+c" = "copy_to_clipboard";
+
         "kitty_mod+v" = "paste_from_clipboard";
         "cmd+v" = "paste_from_clipboard";
+
         "kitty_mod+s" = "";
         "shift+insert" = "";
+
         "kitty_mod+o" = "open_url_with_hints";
       }
       // {
         # scrolling
 
-        # "kitty_mod+up" = "scroll_line_up";
+        "kitty_mod+up" = "scroll_line_up";
         "kitty_mod+k" = "scroll_line_up";
         "opt+cmd+page_up" = "scroll_line_up";
         "cmd+up" = "scroll_line_up";
-        # "kitty_mod+down" = "scroll_line_down";
+
+        "kitty_mod+down" = "scroll_line_down";
         "kitty_mod+j" = "scroll_line_down";
         "opt+cmd+page_down" = "scroll_line_down";
         "cmd+down" = "scroll_line_down";
+
         "kitty_mod+page_up" = "scroll_page_up";
         "cmd+page_up" = "scroll_page_up";
+
         "kitty_mod+page_down" = "scroll_page_down";
         "cmd+page_down" = "scroll_page_down";
+
         "kitty_mod+home" = "scroll_home";
         "cmd+home" = "scroll_home";
+
         "kitty_mod+end" = "scroll_end";
         "cmd+end" = "scroll_end";
-        "kitty_mod+z" = "";
-        "kitty_mod+u" = "scroll_to_prompt -1";
-        # "kitty_mod+up" = "scroll_to_prompt -1"; # was 'scroll to previous prompt'
-        "kitty_mod+x" = "";
-        "kitty_mod+d" = "scroll_to_prompt 1";
-        # "kitty_mod+down" = "scroll_to_prompt 1"; # was 'scroll to next prompt'
+
+        # "kitty_mod+z" = "";
+        # "kitty_mod+u" = "scroll_to_prompt -1";
+
+        # "kitty_mod+x" = "";
+        # "kitty_mod+d" = "scroll_to_prompt 1";
+
         # "kitty_mod+h" = ""; # was 'show scrollback'
         # "kitty_mod+g" = ""; # was 'show last command output'
       }
@@ -62,39 +71,55 @@
 
         "kitty_mod+enter" = "new_window";
         "cmd+enter" = "new_window";
+
         "kitty_mod+n" = "new_os_window";
         "cmd+n" = "new_os_window";
+
         "kitty_mod+w" = "close_window";
         "shift+cmd+d" = "close_window";
+
         "kitty_mod+]" = "next_window";
-        # "alt+]" = "next_window";
+
         "kitty_mod+[" = "prev_window";
-        # "alt+[" = "prev_window";
+
         "kitty_mod+f" = "";
         "kitty_mod+b" = "";
         "kitty_mod+`" = "";
+
         "kitty_mod+r" = ""; # was 'window resize'
         "cmd+r" = "";
+
         "kitty_mod+1" = "";
         "cmd+1" = "";
+
         "kitty_mod+2" = "";
         "cmd+2" = "";
+
         "kitty_mod+3" = "";
         "cmd+3" = "";
+
         "kitty_mod+4" = "";
         "cmd+4" = "";
+
         "kitty_mod+5" = "";
         "cmd+5" = "";
+
         "kitty_mod+6" = "";
         "cmd+6" = "";
+
         "kitty_mod+7" = "";
         "cmd+7" = "";
+
         "kitty_mod+8" = "";
         "cmd+8" = "";
+
         "kitty_mod+9" = "";
         "cmd+9" = "";
+
         "kitty_mod+0" = "";
+
         "kitty_mod+f7" = "";
+
         "kitty_mod+f8" = "";
       }
       // {
@@ -103,14 +128,18 @@
         "kitty_mod+right" = "next_tab";
         "shift+cmd+]" = "next_tab";
         "ctrl+tab" = "next_tab";
+
         "kitty_mod+left" = "previous_tab";
         "shift+cmd+[" = "previous_tab";
         "kitty_mod+tab" = "previous_tab";
+
         "kitty_mod+t" = "new_tab";
         "cmd+t" = "new_tab";
+
         "kitty_mod+q" = "";
         # "cmd+w" = "";
         "shift+cmd+w" = "close_os_window";
+
         "kitty_mod+." = "";
         "kitty_mod+," = "";
         "kitty_mod+alt+t" = "";
@@ -120,7 +149,15 @@
         # layout management
 
         "kitty_mod+l" = ""; # was 'next layout'
-        # "shift+alt+f" = "next_layout";
+        "shift+alt+l" = "next_layout";
+
+        "shift+alt+s" = "goto_layout stack";
+        "shift+alt+t" = "goto_layout tall";
+        "shift+alt+f" = "goto_layout fat";
+        "shift+alt+g" = "goto_layout grid";
+        "shift+alt+x" = "goto_layout splits";
+        "shift+alt+v" = "goto_layout horizontal";
+        "shift+alt+h" = "goto_layout vertical";
       }
       // {
         # font sizes
@@ -131,10 +168,12 @@
         "cmd+plus" = "change_font_size all +2.0";
         "cmd+equal" = "change_font_size all +2.0";
         "shift+cmd+equal" = "change_font_size all +2.0";
+
         "kitty_mod+minus" = "change_font_size all -2.0";
         "kitty_mod+kp_subtract" = "change_font_size all -2.0";
         "cmd+minus" = "change_font_size all -2.0";
         "shift+cmd+minus" = "change_font_size all -2.0";
+
         "kitty_mod+backspace" = "change_font_size all 0";
         "cmd+0" = "change_font_size all 0";
       }
@@ -162,7 +201,7 @@
         "ctrl+cmd+space" = "";
         "kitty_mod+f2" = "";
         "cmd+," = "";
-        "kitty_mod+escape" = "";
+        # "kitty_mod+escape" = "";
         "kitty_mod+a>m" = "";
         "kitty_mod+a>l" = "";
         "kitty_mod+a>1" = "";
