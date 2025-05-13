d79903d664b5ad6bb590cd618570ff5af5805211
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-05-12 20:12:24 -0400

    "Bump up tasks for gremlin"

--- configurations/darwin/gremlin/hercules-ci-agent/agent.nix
+++ configurations/darwin/gremlin/hercules-ci-agent/agent.nix
@@ -8,7 +8,7 @@
   # point the darwin CI agent to our secrets' runtime decryption paths.
   services.hercules-ci-agent = {
     settings = {
-      concurrentTasks = 6;
+      concurrentTasks = 8;
       binaryCachesPath = config.age.secrets."gremlin/binary-caches.json".path;
       clusterJoinTokenPath = config.age.secrets."gremlin/cluster-join-token.key".path;
     };
