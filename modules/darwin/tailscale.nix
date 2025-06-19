53b214db257632443b05206ac6a233dda6382629
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-06-19 00:07:21 -0400

    Try tracking unstable branch for tailscale on macos

--- modules/darwin/tailscale.nix
+++ modules/darwin/tailscale.nix
@@ -1,6 +1,11 @@
 {
+  withSystem,
+  pkgs,
+  ...
+}: {
   # homebrew.casks = ["tailscale"];
   services.tailscale = {
     enable = true;
+    package = withSystem pkgs.stdenv.system ({inputs', ...}: inputs'.nixpkgs-unstable.legacyPackages.tailscale);
   };
 }
