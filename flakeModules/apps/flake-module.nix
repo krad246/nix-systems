e86b2d166d622247a6853659bd635cfd8b0eed3f
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-10-04 12:16:47 -0700

    Add some app targets to fix borked nix run targets

--- flakeModules/apps/flake-module.nix
+++ flakeModules/apps/flake-module.nix
@@ -1,5 +1,17 @@
 # outer / 'flake' scope
-_: {
+{withSystem, ...}: {
+  flake.apps = {
+    aarch64-darwin = {
+      fortress-disko-vm = let
+        vm = withSystem "aarch64-darwin" ({self', ...}: self'.packages.fortress-disko-vm);
+      in {
+        type = "app";
+        program = "${vm}/disko-vm";
+        meta.description = "Run a disko-images VM based on the fortress configuration.";
+      };
+    };
+  };
+
   perSystem = ctx @ {
     config,
     lib,
