971f709b84ddd6899191cbfa0991697f9c6ecf3f
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-10-12 21:33:51 -0700

    declutter darwin/base-configuration/default.nix

---
+++ modules/darwin/base-configuration/misc-prefs.nix
@@ -0,0 +1,16 @@
+{config, ...}: {
+  system.defaults = {
+    NSGlobalDomain = {
+      NSDisableAutomaticTermination = false;
+      NSDocumentSaveNewDocumentsToCloud = true;
+
+      "com.apple.sound.beep.volume" = 0.5;
+      "com.apple.springing.enabled" = true;
+    };
+  };
+
+  networking = {
+    localHostName = config.networking.hostName;
+    computerName = config.networking.hostName;
+  };
+}
