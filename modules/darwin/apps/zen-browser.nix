c545357ad99c33dd1612774b36f0a65998e5da96
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-10-19 14:39:03 -0700

    Make Zen open HM manual

--- modules/darwin/apps/zen-browser.nix
+++ modules/darwin/apps/zen-browser.nix
@@ -13,5 +13,46 @@ in {
     homebrew = {
       casks = lib.modules.mkIf cfg ["zen"];
     };
+
+    home-manager.sharedModules = lib.lists.optionals cfg [
+      ({
+        config,
+        lib,
+        pkgs,
+        modulesPath,
+        ...
+      }: {
+        manual.html.enable = lib.modules.mkForce false;
+
+        home.packages = let
+          docsPath = modulesPath + "/../docs";
+
+          docs = import docsPath {
+            inherit lib pkgs;
+            inherit (config.home.version) release isReleaseBranch;
+          };
+
+          htmlOpenTool = pkgs.symlinkJoin {
+            name = "home-manager";
+            paths = [docs.manual.htmlOpenTool];
+            buildInputs = [pkgs.makeWrapper];
+            postBuild = let
+              opener = pkgs.writeShellApplication {
+                name = "zen-browser-file-open";
+                text = ''
+                  open -a Zen -u "file://$1"
+                '';
+              };
+            in ''
+              wrapProgram $out/bin/home-manager-help --set BROWSER ${lib.meta.getExe opener}
+            '';
+          };
+        in
+          with pkgs; (with docs.manual; [
+            html
+            htmlOpenTool
+          ]);
+      })
+    ];
   };
 }
