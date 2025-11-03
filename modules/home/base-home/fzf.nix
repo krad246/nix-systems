1e9c974d3314e35e245930db74ef206b1525a999
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-11-02 19:46:53 -0800

    Clean up FZF fd bindings

--- modules/home/base-home/fzf.nix
+++ modules/home/base-home/fzf.nix
@@ -1,6 +1,13 @@
 {
+  config,
+  lib,
+  ...
+}: {
   programs.fzf = {
     enable = true;
     enableBashIntegration = true;
+    defaultCommand = lib.meta.getExe config.programs.fd.package;
+    fileWidgetCommand = lib.meta.getExe config.programs.fd.package;
+    changeDirWidgetCommand = lib.meta.getExe config.programs.fd.package;
   };
 }
