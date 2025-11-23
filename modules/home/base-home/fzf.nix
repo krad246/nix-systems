251539f19130d269eb3523f006dffd4442f200a3
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-11-23 13:12:37 -0800

    Allow suspendable apps to run from inside of FZF!

--- modules/home/base-home/fzf.nix
+++ modules/home/base-home/fzf.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: {
   programs.fzf = {
@@ -31,11 +32,60 @@
         '';
         preview-window = "up";
 
-        bind = [
-          # TODO: consider become(); it's a bit tricky with TTY forwarding though
-          # FIXME: ctrl-z is fundamentally broken with keybinds like these because it causes fzf to deadlock
-          # "ctrl-o:execute(find {+} -type f -print0 | xargs --no-run-if-empty -0 -o $EDITOR)"
-          # "ctrl-v:execute(find {+} -type f -print0 | xargs --no-run-if-empty -0 -o ${lib.meta.getExe config.programs.bat.package})"
+        bind = let
+          wrap = key: cmd: let
+            script = pkgs.writeShellApplication {
+              name = "fzf-${key}-nosusp";
+              text = ''
+                stty susp undef
+                find "$@" -type f -print0 | \
+                  xargs --no-run-if-empty -0 -o "${cmd}"
+              '';
+            };
+            bin = lib.meta.getExe script;
+          in "${key}:execute(${bin} {+})";
+        in [
+          (wrap "ctrl-o" (let
+            hx-nosuspend = pkgs.symlinkJoin rec {
+              name = "hx";
+              paths = [config.programs.helix.package];
+              buildInputs = [pkgs.makeWrapper];
+
+              # Wrap helix to be started with ctrl-z disabled.
+              postBuild = let
+                # Deep rewrite: any attribute named "C-z" becomes "no_op"
+                # TODO: there must be a library function that does this
+                noCZ = let
+                  stripCZ = let
+                    go = v:
+                      if builtins.isAttrs v
+                      then
+                        lib.mapAttrs
+                        (
+                          name: val:
+                            if name == "C-z"
+                            then "no_op"
+                            else go val
+                        )
+                        v
+                      else if lib.isList v
+                      then map go v
+                      else v;
+                  in
+                    go;
+                in
+                  stripCZ config.programs.helix.settings;
+              in ''
+                wrapProgram $out/bin/hx --add-flags '-c ${let
+                  tomlFormat = pkgs.formats.toml {};
+                in
+                  tomlFormat.generate "hx-nosusp-config.toml" noCZ}'
+              '';
+            };
+          in
+            lib.meta.getExe hx-nosuspend))
+          (wrap "ctrl-v" (lib.meta.getExe config.programs.bat.package))
+          "ctrl-r:reload(eval \"$FZF_CTRL_T_COMMAND\")"
         ];
 
         scheme = "path";
