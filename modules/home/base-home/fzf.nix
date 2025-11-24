481ea5463516abf424bb3aa646659e68d2c7000f
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-11-23 22:23:32 -0800

    Refactor FZF module for clarity

--- modules/home/base-home/fzf.nix
+++ modules/home/base-home/fzf.nix
@@ -3,139 +3,154 @@
   lib,
   pkgs,
   ...
-}: {
-  programs.fzf = {
+}: let
+  inherit (lib) cli meta strings;
+in {
+  programs.fzf = let
+    # Construct an FZF keybind string
+    keybind = {
+      key,
+      action,
+    }: "'${key}:${action}'";
+
+    # Construct an FZF action that handles FZF multi-select
+    # Pass the list of files selected to an arbitrary command.
+    on-multiselect = {
+      key,
+      command,
+    }:
+      keybind {
+        inherit key;
+        action = let
+          script = pkgs.writeShellApplication {
+            name = "fzf-${key}-on-multiselect";
+            text = command;
+          };
+        in "execute(find {+} -type f -print0 | xargs --no-run-if-empty -0 -o ${meta.getExe script})";
+      };
+
+    defaultArgs = {
+      height = "~100%";
+      multi = true;
+      highlight-line = true;
+      cycle = true;
+      wrap = true;
+      track = true;
+      filepath-word = true;
+      bind = [
+      ];
+    };
+
+    fileArgs = rec {
+      layout = "reverse-list";
+      scheme = "path";
+
+      preview-window = "up";
+      preview = let
+        fzf-preview = pkgs.unstable.fzf-preview.override {
+          bat = config.programs.bat.package;
+        };
+      in "'${meta.getExe fzf-preview} {}'";
+
+      bind =
+        defaultArgs.bind
+        ++ [
+          (keybind {
+            key = "ctrl-/";
+            action = "change-preview-window(hidden|${preview-window})";
+          })
+          (keybind {
+            key = "ctrl-r";
+            action = "reload(eval '${config.programs.fzf.defaultCommand}')";
+          })
+        ];
+    };
+  in {
     enable = true;
     enableBashIntegration = true;
-    defaultCommand = ''
-      ${lib.meta.getExe config.programs.fd.package}
-    '';
 
-    fileWidgetCommand = ''
-      ${lib.meta.getExe config.programs.fd.package}
-    '';
+    changeDirWidgetCommand = strings.concatStringsSep " " [
+      (meta.getExe config.programs.fd.package)
+      (cli.toGNUCommandLineShell {} {
+        type = "d";
+      })
+    ];
 
-    fileWidgetOptions = let
-      args = lib.cli.toGNUCommandLineShell {} {
-        height = "~100%";
-        layout = "reverse-list";
+    changeDirWidgetOptions = cli.toGNUCommandLine {} (defaultArgs // fileArgs);
 
-        multi = true;
-        highlight-line = true;
-        cycle = true;
-        wrap = true;
-        track = true;
-        filepath-word = true;
+    defaultCommand = meta.getExe config.programs.fd.package;
+    defaultOptions = cli.toGNUCommandLine {} defaultArgs;
 
-        preview = ''
-          ${lib.meta.getExe (pkgs.unstable.fzf-preview.override {bat = config.programs.bat.package;})} {}
-        '';
-        preview-window = "up";
-
-        bind = let
-          wrap = key: cmd: let
-            script = pkgs.writeShellApplication {
-              name = "fzf-${key}-nosusp";
-              text = ''
+    fileWidgetOptions = cli.toGNUCommandLine {} (
+      defaultArgs
+      // fileArgs
+      // {
+        bind =
+          fileArgs.bind
+          ++ [
+            (on-multiselect {
+              key = "ctrl-v";
+              command = ''
                 stty susp undef
-                find "$@" -type f -print0 | \
-                  xargs --no-run-if-empty -0 -o ${cmd}
+                exec ${strings.concatStringsSep " " [
+                  (meta.getExe config.programs.bat.package)
+                  (cli.toGNUCommandLineShell {} {
+                    paging = "always";
+                  })
+                ]} "$@"
               '';
-            };
-            bin = lib.meta.getExe script;
-          in "${key}:execute(${bin} -- {+})";
-        in [
-          (wrap "ctrl-o" (let
-            hx-nosuspend = pkgs.symlinkJoin rec {
-              name = "hx";
-              paths = [config.programs.helix.package];
-              buildInputs = [pkgs.makeWrapper];
+            })
+            (on-multiselect {
+              key = "ctrl-o";
+              command = let
+                hx-no-ctrl-z = pkgs.symlinkJoin {
+                  name = "hx";
+                  paths = [config.programs.helix.package];
+                  buildInputs = [pkgs.makeWrapper];
 
-              # Wrap helix to be started with ctrl-z disabled.
-              postBuild = let
-                # Deep rewrite: any attribute named "C-z" becomes "no_op"
-                # TODO: there must be a library function that does this
-                noCZ = let
-                  stripCZ = let
-                    go = v:
-                      if builtins.isAttrs v
-                      then
-                        lib.mapAttrs
-                        (
-                          name: val:
-                            if name == "C-z"
-                            then "no_op"
-                            else go val
-                        )
-                        v
-                      else if lib.isList v
-                      then map go v
-                      else v;
-                  in
-                    go;
-                in
-                  stripCZ config.programs.helix.settings;
+                  # nosusp helix to be started with ctrl-z disabled.
+                  postBuild = let
+                    # Deep rewrite: any attribute named "C-z" becomes "no_op"
+                    # TODO: there must be a library function that does this
+                    noCZ = let
+                      stripCZ = let
+                        go = v:
+                          if builtins.isAttrs v
+                          then
+                            lib.mapAttrs
+                            (
+                              name: val:
+                                if name == "C-z"
+                                then "no_op"
+                                else go val
+                            )
+                            v
+                          else if lib.isList v
+                          then map go v
+                          else v;
+                      in
+                        go;
+                    in
+                      stripCZ config.programs.helix.settings;
+                  in ''
+                    wrapProgram $out/bin/hx --add-flags '-c ${let
+                      tomlFormat = pkgs.formats.toml {};
+                    in
+                      tomlFormat.generate "hx-nosusp-config.toml" noCZ}'
+                  '';
+                };
               in ''
-                wrapProgram $out/bin/hx --add-flags '-c ${let
-                  tomlFormat = pkgs.formats.toml {};
-                in
-                  tomlFormat.generate "hx-nosusp-config.toml" noCZ}'
+                stty susp undef
+                exec ${meta.getExe hx-no-ctrl-z} "$@"
               '';
-            };
-          in
-            lib.meta.getExe hx-nosuspend))
-          (wrap "ctrl-v" "${lib.meta.getExe config.programs.bat.package} --paging always")
-          "ctrl-r:reload(eval \"$FZF_CTRL_T_COMMAND\")"
-          "ctrl-/:change-preview-window(hidden|up)"
-        ];
+            })
+          ];
+      }
+    );
 
-        scheme = "path";
-      };
-    in [
-      args
-    ];
-
-    historyWidgetOptions = let
-      args = lib.cli.toGNUCommandLineShell {} {
-        height = "~100%";
-        # layout = "reverse-list";
-
-        multi = true;
-        highlight-line = true;
-        cycle = true;
-        wrap = true;
-        track = true;
-        filepath-word = true;
-
+    historyWidgetOptions = cli.toGNUCommandLine {} (defaultArgs
+      // {
         scheme = "history";
-      };
-    in [
-      args
-    ];
-
-    changeDirWidgetCommand = ''
-      ${lib.meta.getExe config.programs.fd.package} --type d
-    '';
-
-    changeDirWidgetOptions = let
-      args = lib.cli.toGNUCommandLineShell {} {
-        height = "~100%";
-        layout = "reverse-list";
-
-        multi = true;
-        highlight-line = true;
-        cycle = true;
-        wrap = true;
-        track = true;
-        filepath-word = true;
-
-        scheme = "path";
-
-        preview = ''
-          ${lib.meta.getExe (pkgs.unstable.fzf-preview.override {bat = config.programs.bat.package;})} {}
-        '';
-        preview-window = "up";
-      };
-    in [args];
+      });
   };
 }
