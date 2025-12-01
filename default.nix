c3bcd33c478cbef3602dfb2d01d91a8529dedd55
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-11-30 18:21:56 -0800

    Clean up flake-compat idioms (no custom code)

--- default.nix
+++ default.nix
@@ -1,21 +1,14 @@
-{
-  system ? builtins.currentSystem,
-  flake ? true,
-  ...
-}: let
-  flake-compat = let
-    lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-  in
-    fetchTarball {
-      url = lock.nodes.flake-compat.locked.url or "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-      sha256 = lock.nodes.flake-compat.locked.narHash;
-    };
-
-  load-flake =
-    import flake-compat {src = ./.;};
-
-  inherit (load-flake) defaultNix;
-in
-  if flake
-  then defaultNix
-  else defaultNix.outputs.legacyPackages.${system}
+(
+  import
+  (
+    let
+      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+      nodeName = lock.nodes.root.inputs.flake-compat;
+    in
+      fetchTarball {
+        url = lock.nodes.${nodeName}.locked.url or "https://github.com/edolstra/flake-compat/archive/${lock.nodes.${nodeName}.locked.rev}.tar.gz";
+        sha256 = lock.nodes.${nodeName}.locked.narHash;
+      }
+  )
+  {src = ./.;}
+).defaultNix
