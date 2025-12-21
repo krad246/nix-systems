82c46fb127402732127e28611c9683781ea86319
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-12-20 17:39:51 -0800

    Clean up the disko-vm formatConfigs

--- configurations/nixos/fortress/specialisations/formats/disko-vm.nix
+++ configurations/nixos/fortress/specialisations/formats/disko-vm.nix
@@ -1,5 +1,10 @@
 {
   formatAttr = "vmWithDisko";
   virtualisation.vmVariantWithDisko = {
+    boot.initrd.kernelModules = [
+      "virtio_pci"
+      "virtio_net"
+      "virtio_blk"
+    ];
   };
 }
