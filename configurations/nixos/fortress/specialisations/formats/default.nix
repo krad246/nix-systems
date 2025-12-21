1c3017da9a567303f5b3f454fe8bb5d5df2a4f16
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-12-20 18:21:51 -0800

    Deactivate mkQemuConfig options

--- configurations/nixos/fortress/specialisations/formats/default.nix
+++ configurations/nixos/fortress/specialisations/formats/default.nix
@@ -14,27 +14,25 @@ let
     };
   in
     module;
-
-  mkQemuConfig = extraConfig: let
-    module = {modulesPath, ...}: {
-      imports =
-        [
-          (modulesPath + "/virtualisation/qemu-vm.nix")
-        ]
-        ++ [
-          (mkConfig extraConfig)
-        ];
-
-      virtualisation = {
-        cores = 6;
-        memorySize = 8 * 1024;
-      };
-    };
-  in
-    module;
+  # mkQemuConfig = extraConfig: let
+  #   module = {modulesPath, ...}: {
+  #     imports =
+  #       [
+  #         (modulesPath + "/virtualisation/qemu-vm.nix")
+  #       ]
+  #       ++ [
+  #         (mkConfig extraConfig)
+  #       ];
+  #     virtualisation = {
+  #       cores = 6;
+  #       memorySize = 8 * 1024;
+  #     };
+  #   };
+  # in
+  #   module;
 in {
-  disko-vm = mkQemuConfig ./disko-vm.nix;
-  disko-vm-darwin = mkQemuConfig ./disko-vm-darwin.nix;
+  disko-vm = mkConfig ./disko-vm.nix;
+  disko-vm-darwin = mkConfig ./disko-vm-darwin.nix;
   hyperv = mkConfig ./hyperv.nix;
   install-iso-hyperv = mkConfig ./install-iso-hyperv.nix;
   install-iso = mkConfig ./install-iso.nix;
@@ -48,6 +46,6 @@ in {
   sd-x86_64 = mkConfig ./sd-x86_64.nix;
   vagrant-virtualbox = mkConfig ./vagrant-virtualbox.nix;
   virtualbox = mkConfig ./virtualbox.nix;
-  vm = mkQemuConfig ./vm.nix;
+  vm = mkConfig ./vm.nix;
   vmware = mkConfig ./vmware.nix;
 }
