8fde82845729e43eca18282986b1e4c338d974b5
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-12-23 00:34:29 -0800

    Add core count to disko-vm

--- configurations/nixos/fortress/specialisations/formats/disko-vm.nix
+++ configurations/nixos/fortress/specialisations/formats/disko-vm.nix
@@ -1,12 +1,11 @@
 {
   formatAttr = "vmWithDisko";
   virtualisation.vmVariantWithDisko = {
+    imports = [./vm.nix];
     boot.initrd.kernelModules = [
       "virtio_pci"
       "virtio_net"
       "virtio_blk"
     ];
-
-    virtualisation.cores = 8;
   };
 }
