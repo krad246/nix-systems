3605c3891e4fd43b5008b3e622b0b89190ef0281
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-12-24 23:55:59 -0800

    Correct broken app targets; identify a flake module synergy between flake output domains

--- flakeModules/apps/flake-module.nix
+++ flakeModules/apps/flake-module.nix
@@ -7,40 +7,42 @@
     pkgs,
     ...
   }: {
-    apps = {
-      bootstrap = let
-        runner = pkgs.callPackage ./bootstrap.nix {
-          flake-root = config.flake-root.package;
+    apps =
+      {
+        bootstrap = let
+          runner = pkgs.callPackage ./bootstrap.nix {
+            flake-root = config.flake-root.package;
+          };
+        in {
+          type = "app";
+          program = lib.meta.getExe runner;
+          meta.description = "Run the devShell bootstrap script.";
         };
-      in {
-        type = "app";
-        program = lib.meta.getExe runner;
-        meta.description = "Run the devShell bootstrap script.";
-      };
 
-      fortress-disko-vm = let
-        vm = self'.packages.fortress-disko-vm;
-      in {
-        type = "app";
-        program = "${vm}/disko-vm";
-        meta.description = "Run a disko-images VM based on the fortress configuration.";
-      };
+        fortress-disko-vm = let
+          vm = self'.packages.fortress-disko-vm;
+        in {
+          type = "app";
+          program = "${vm}/disko-vm";
+          meta.description = "Run a disko-images VM based on the fortress configuration.";
+        };
+      }
+      // lib.attrsets.optionalAttrs pkgs.stdenv.isLinux {
+        fortress-vm = let
+          vm = self'.packages.fortress-vm;
+        in {
+          type = "app";
+          program = "${vm}/run-fortress-vm";
+          meta.description = "Run a VM based on the fortress configuration.";
+        };
 
-      fortress-vm = let
-        vm = self'.packages.fortress-vm;
-      in {
-        type = "app";
-        program = "${vm}/run-fortress-vm";
-        meta.description = "Run a VM based on the fortress configuration.";
+        fortress-vm-bootloader = let
+          vm = self'.packages.fortress-vm-bootloader;
+        in {
+          type = "app";
+          program = "${vm}/run-fortress-vm";
+          meta.description = "Run a VM based on the fortress configuration, with bootloader.";
+        };
       };
-
-      fortress-vm-bootloader = let
-        vm = self'.packages.fortress-vm-bootloader;
-      in {
-        type = "app";
-        program = "${vm}/run-fortress-vm";
-        meta.description = "Run a VM based on the fortress configuration, with bootloader.";
-      };
-    };
   };
 }
