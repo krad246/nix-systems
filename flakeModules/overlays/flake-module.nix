03b3b56ba54e7abd4696dd75534dce16dda83b69
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-12-26 15:11:29 -0800

    Works if I don't do a pkgs.extend.

--- flakeModules/overlays/flake-module.nix
+++ flakeModules/overlays/flake-module.nix
@@ -3,24 +3,31 @@
   withSystem,
   inputs,
   self,
+  lib,
   ...
 }: {
-  imports = [
-    inputs.flake-parts.flakeModules.easyOverlay
-  ];
-
   flake = let
     ext = import ./lib;
   in {
-    lib = inputs.nixpkgs.lib.extend ext;
+    lib = lib.extend ext;
 
     overlays = {
-      # Unstable package set
-      unstable = _final: prev: {
-        unstable = import inputs.nixpkgs-unstable {
-          inherit (prev) config overlays system;
-        };
-      };
+      default = lib.fixedPoints.composeManyExtensions [
+        self.overlays.flake
+        self.overlays.krad246
+        self.overlays.unstable
+      ];
+
+      # This flake's outputs
+      flake = _final: prev:
+        withSystem prev.stdenv.hostPlatform.system (
+          {config, ...}: {
+            flake = {
+              flake-root = config.flake-root.package;
+              inherit (config) formatter;
+            };
+          }
+        );
 
       # krad246 packages and custom behaviors
       krad246 = _final: prev:
@@ -46,36 +53,12 @@
           }
         );
 
-      # This flake's outputs
-      flake = _final: prev:
-        withSystem prev.stdenv.hostPlatform.system (
-          {config, ...}: {
-            flake = {
-              flake-root = config.flake-root.package;
-              inherit (config) formatter;
-            };
-          }
-        );
-    };
-  };
-
-  perSystem = {
-    lib,
-    pkgs,
-    ...
-  }: {
-    overlayAttrs = let
-      inherit (lib) fixedPoints;
-      pkgs' = pkgs.extend (fixedPoints.composeManyExtensions [
-        self.overlays.flake
-        self.overlays.krad246
-        self.overlays.unstable
-      ]);
-    in {
-      inherit (pkgs') flake;
-      inherit (pkgs') lib;
-      inherit (pkgs') krad246;
-      inherit (pkgs') unstable;
+      # Unstable package set
+      unstable = _final: prev: {
+        unstable = import inputs.nixpkgs-unstable {
+          inherit (prev) config overlays system;
+        };
+      };
     };
   };
 }
