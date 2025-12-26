646d1bb92af5b119e76feb88150771369deb0be4
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-12-26 15:25:46 -0800

    Correct the easyOverlay issue; connect my overlays to an adapter

--- flakeModules/overlays/flake-module.nix
+++ flakeModules/overlays/flake-module.nix
@@ -6,17 +6,29 @@
   lib,
   ...
 }: {
+  imports = [inputs.flake-parts.flakeModules.easyOverlay];
+
+  perSystem = {
+    pkgs,
+    final,
+    ...
+  }: {
+    overlayAttrs = self.overlays.combined final pkgs;
+  };
+
   flake = let
     ext = import ./lib;
   in {
     lib = lib.extend ext;
 
     overlays = {
-      default = lib.fixedPoints.composeManyExtensions [
-        self.overlays.flake
-        self.overlays.krad246
-        self.overlays.unstable
-      ];
+      combined = lib.fixedPoints.composeManyExtensions ([
+          self.overlays.unstable
+        ]
+        ++ [
+          self.overlays.flake
+          self.overlays.krad246
+        ]);
 
       # This flake's outputs
       flake = _final: prev:
