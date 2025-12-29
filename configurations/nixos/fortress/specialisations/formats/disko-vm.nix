dffdbf29558a26ebfaea394f60640ad647ae9ae6
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2025-12-29 00:04:02 -0800

    Nearly builds with impermanence on disko-vm

--- configurations/nixos/fortress/specialisations/formats/disko-vm.nix
+++ configurations/nixos/fortress/specialisations/formats/disko-vm.nix
@@ -1,11 +1,36 @@
 {
+  self,
+  lib,
+  ...
+}: {
   formatAttr = "vmWithDisko";
   virtualisation.vmVariantWithDisko = {
-    imports = [./vm.nix];
+    imports = [./vm.nix] ++ [self.diskoConfigurations.fortress-desktop];
     boot.initrd.kernelModules = [
       "virtio_pci"
       "virtio_net"
       "virtio_blk"
     ];
+
+    disko.devices = {
+      disk.main = {
+        device = "/dev/vda";
+        content.partitions."ESP".content.mountpoint = lib.modules.mkForce "/boot";
+      };
+      lvm_vg.pool.lvs = {
+        home.content.mountpoint = lib.modules.mkForce "/home";
+        nix.content.mountpoint = lib.modules.mkForce "/nix";
+        persist.content.mountpoint = lib.modules.mkForce "/nix/persist";
+      };
+    };
+
+    boot.loader = {
+      grub = {
+        enable = true;
+        efiSupport = true;
+        device = "nodev";
+      };
+    };
+    virtualisation.fileSystems."/nix/persist".neededForBoot = true;
   };
 }
