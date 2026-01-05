6660b9574fcd90e58b1e27cc420c95e1dbe41e27
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2026-01-04 16:20:39 -0800

    Upgrade stub-disko to an LVM disk setup (try to get to representative impermanence)

--- configurations/nixos/fortress/stub-disko.nix
+++ configurations/nixos/fortress/stub-disko.nix
@@ -10,14 +10,23 @@ in {
     inputs.disko.nixosModules.disko
   ];
 
-  disko.devices = modules.mkDefault {
+  disko.devices = {
     disk = {
       main = {
-        device = "/dev/vda";
         type = "disk";
+
+        device = modules.mkDefault "/dev/vda";
+        imageSize = "24G"; # for VMs
+
         content = {
           type = "gpt";
+
           partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+            };
+
             ESP = {
               type = "EF00";
               size = "100M";
@@ -27,34 +36,66 @@ in {
                 mountpoint = "/boot";
               };
             };
-            empty = {
-              size = "1G";
-            };
 
-            root = {
+            # Override to LUKS or other desired layout.
+            # We'll enforce a strict LVM architecture because of its flexibility as an interface.
+            root = modules.mkDefault {
               size = "100%";
               content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-              };
-            };
-
-            swap = {
-              size = "2G";
-              content = {
-                type = "swap";
-                priority = 100;
-                resumeDevice = true;
+                type = "lvm_pv";
+                vg = "pool";
               };
             };
           };
         };
+      };
+    };
 
-        imageSize = "24G";
+    lvm_vg.pool = {
+      type = "lvm_vg";
+
+      lvs = modules.mkDefault {
+        root = {
+          size = "100%";
+          content = {
+            type = "filesystem";
+            format = "ext4";
+            mountpoint = "/";
+          };
+        };
+
+        home = {
+          size = "20%VG";
+          content = {
+            type = "filesystem";
+            format = "ext4";
+            mountpoint = "/home";
+          };
+        };
+
+        # nix = {
+        #   size = "45%VG";
+        #   content = {
+        #     type = "filesystem";
+        #     format = "ext4";
+        #     mountpoint = "/nix";
+        #   };
+        # };
+
+        swap = {
+          size = "5%VG";
+          content = {
+            type = "swap";
+            priority = 100;
+            resumeDevice = true;
+          };
+        };
       };
     };
   };
 
-  boot.loader.grub.device = modules.mkDefault config.disko.devices.disk.main.device;
+  boot.loader.grub = {
+    inherit (config.disko.devices.disk.main) device;
+    efiSupport = true;
+  };
 }
