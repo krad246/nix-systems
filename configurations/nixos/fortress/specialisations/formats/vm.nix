93f132ec6205b1ecb6d3c842f08514c6e0e38aa4
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2026-01-09 22:58:58 -0800

    Reapply "Try to push the enableConfig stuff into the vmVariant modules"
    This reverts commit 497aea77d350a2ebba356620fd35d991bbb28316.

--- configurations/nixos/fortress/specialisations/formats/vm.nix
+++ configurations/nixos/fortress/specialisations/formats/vm.nix
@@ -1,7 +1,6 @@
 {lib, ...}: {
-  disko.enableConfig = lib.modules.mkForce false;
   virtualisation = {
     cores = 8;
-    # vmVariant.disko.enableConfig = lib.modules.mkForce false;
+    vmVariant.disko.enableConfig = lib.modules.mkForce false;
   };
 }
