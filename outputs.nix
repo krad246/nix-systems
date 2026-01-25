a162ceafb4bb726932ca85f2436fdfa9d53c59cf
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2026-01-25 00:31:12 -0800

    Add flake-file infrastructure

--- outputs.nix
+++ outputs.nix
@@ -39,7 +39,115 @@ in {
     ]
     ++ [
       flake-parts.flakeModules.partitions
+    ]
+    ++ [
+      inputs.flake-file.flakeModules.default
     ];
+
+  flake-file = {
+    nixConfig = {
+      extra-experimental-features = "nix-command flakes";
+      extra-substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+        "https://krad246.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "krad246.cachix.org-1:N57J9SfNFtxMSYnlULH4l7ZkdNjIQb0ByyapaEb/8IM="
+      ];
+    };
+
+    inputs = {
+      systems.url = "github:nix-systems/default";
+
+      nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+      nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+      # WSL distribution on NixOS
+      nixos-wsl.url = "github:nix-community/nixos-wsl/main";
+
+      # Darwin shims for Nix
+      darwin.url = "github:lnl7/nix-darwin/nix-darwin-25.05";
+
+      # Cross-platform (Linux / MacOS) userspace package management
+      home-manager.url = "github:nix-community/home-manager/release-25.05";
+
+      # Flake-Parts module gluing it together
+      ez-configs.url = "github:ehllie/ez-configs";
+
+      # Hardware platform configurations with options preset
+      nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
+      # Simple modules for generating a variety of image formats
+      nixos-generators.url = "github:nix-community/nixos-generators";
+
+      # Immutable OS root filesystem (erase your darlings)
+      impermanence.url = "github:nix-community/impermanence";
+
+      # Declarative disk partitioning
+      disko.url = "github:nix-community/disko";
+
+      # Nix User Repositories
+      # out-of-band package management
+      nur.url = "github:nix-community/NUR";
+      nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+      nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=v0.4.1";
+
+      # Legacy and flake compatibility shims.
+      flake-compat.url = "github:edolstra/flake-compat";
+
+      # An opinionated Nix flake library (see flake-utils)
+      flake-parts.url = "github:hercules-ci/flake-parts";
+
+      flake-file.url = "github:vic/flake-file";
+
+      # Simple connection glue between direnv, nix-shell, and flakes to get
+      # the absolute roots of various subflakes in a project.
+      flake-root.url = "github:srid/flake-root";
+
+      # Consumer flake to build all outputs in this flake
+      devour-flake = {
+        url = "github:srid/devour-flake";
+        flake = false;
+      };
+
+      # Glue logic between just and Nix (replacement to mission-control)
+      just-flake.url = "github:juspay/just-flake";
+
+      # Swiss-army-knife formatter.
+      treefmt-nix.url = "github:numtide/treefmt-nix";
+
+      # Code cleanliness checking for developers.
+      pre-commit-hooks-nix.url = "github:cachix/pre-commit-hooks.nix";
+
+      # hercules CI support
+      hercules-ci-agent.url = "github:hercules-ci/hercules-ci-agent/hercules-ci-agent-0.10.5";
+      hercules-ci-effects.url = "github:hercules-ci/hercules-ci-effects";
+
+      # Handles the Spotlight and Dock synchronization
+      mac-app-util.url = "github:hraban/mac-app-util";
+
+      # AGE encrypted secrets
+      # Handle rekeying via Yubikey, etc.
+      agenix.url = "github:ryantm/agenix";
+      agenix-rekey.url = "github:oddlama/agenix-rekey";
+      agenix-shell.url = "github:aciceri/agenix-shell";
+
+      # window manager stuff
+
+      nixos-cosmic.url = "github:lilyinstarlight/nixos-cosmic";
+
+      dconf2nix = {
+        url = "github:nix-community/dconf2nix/master";
+        flake = false;
+      };
+
+      vscode-server.url = "github:nix-community/nixos-vscode-server";
+      nixGL.url = "github:nix-community/nixGL";
+    };
+  };
 
   flake = rec {
     flakeModules = {
