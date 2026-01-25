2d50c459125c2b5dde9f693a07b52af431420b06
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2026-01-24 23:26:31 -0800

    Clean flake-parts baseline

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake
+    {
+      inherit inputs;
+    }
+    {
+      imports = [];
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
+    };
+}
