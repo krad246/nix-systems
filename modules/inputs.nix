f63980f21cac27b6a286cf2a55c5a34f35a38dd6
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2026-01-25 00:47:34 -0800

    Add lock pruning

--- modules/inputs.nix
+++ modules/inputs.nix
@@ -10,6 +10,8 @@
       flake-parts.url = "github:hercules-ci/flake-parts";
       flake-file.url = "github:vic/flake-file";
     };
+
+    prune-lock.enable = true;
   };
 
   systems = import inputs.systems;
