af3fe30bf2adc41c857c655e4ccb447ece55958e
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2026-02-17 22:35:10 -0800

    WIP: Add Zen Browser dendritic module

---
+++ modules/web-browsers/zen-browser.nix
@@ -0,0 +1,33 @@
+{
+  flake.modules = {
+    darwin.zen-browser = {
+      homebrew.casks = ["zen"];
+    };
+
+    homeManager.zen-browser = {
+      lib,
+      pkgs,
+      ...
+    }: {
+      config = lib.modules.mkMerge [
+        (lib.modules.mkIf pkgs.stdenv.hostPlatform.isDarwin {
+          home.sessionVariables.BROWSER = let
+            opener = pkgs.writeShellApplication {
+              name = "zen-browser-open";
+              text = ''
+                exec open -a Zen "$@"
+              '';
+            };
+          in
+            lib.meta.getExe opener;
+        })
+        (lib.modules.mkIf pkgs.stdenv.hostPlatform.isLinux {
+          services.flatpak.packages = ["app.zen_browser.zen"];
+        })
+      ];
+    };
+
+    nixos.zen-browser = {
+    };
+  };
+}
