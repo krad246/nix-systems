421214721a93dc81985bc7f2df2eb91c9918dd45
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2026-02-22 12:51:43 -0800

    Add shell interface modules

---
+++ modules/shell/backends/bash.nix
@@ -0,0 +1,139 @@
+{
+  flake.modules.homeManager.shell = {
+    config,
+    lib,
+    pkgs,
+    ...
+  }: let
+    cfg = config.shell;
+  in {
+    options.shell.backends.bash = {
+      enable = lib.options.mkEnableOption "Whether to manage `bash`.";
+      integrations = {
+        batpipe.enable =
+          lib.options.mkEnableOption "Whether to enable the `batpipe` integration for `bash`."
+          // {
+            internal = true;
+          };
+        direnv.enable =
+          lib.options.mkEnableOption "Whether to enable the `direnv` integration for `bash`."
+          // {
+            default = cfg.integrations.direnv.enable;
+          };
+        fzf.enable =
+          lib.options.mkEnableOption "Whether to enable the `fzf` integration for `bash`."
+          // {
+            default = cfg.integrations.fzf.enable;
+          };
+        lsd.enable =
+          lib.options.mkEnableOption "Whether to enable the `lsd` integration for `bash`."
+          // {
+            default = cfg.integrations.lsd.enable;
+          };
+        kitty.enable =
+          lib.options.mkEnableOption "Whether to enable the `kitty` integration for `bash`."
+          // {
+            default = cfg.integrations.kitty.enable;
+          };
+        starship.enable =
+          lib.options.mkEnableOption "Whether to enable the `starship` integration for `bash`."
+          // {
+            default = cfg.integrations.starship.enable;
+          };
+        yazi.enable =
+          lib.options.mkEnableOption "Whether to enable the `yazi` integration for `bash`."
+          // {
+            default = cfg.integrations.yazi.enable;
+          };
+        zoxide.enable =
+          lib.options.mkEnableOption "Whether to enable the `zoxide` integration for `bash`."
+          // {
+            default = cfg.integrations.zoxide.enable;
+          };
+      };
+    };
+
+    config = lib.modules.mkIf cfg.backends.bash.enable (lib.modules.mkMerge [
+      {
+        programs = {
+          bash = {
+            enable = true;
+            enableCompletion = true;
+            enableVteIntegration = true;
+
+            initExtra = ''
+              set -o vi
+            '';
+
+            historyControl = [
+              "erasedups"
+              "ignoreboth"
+            ];
+
+            historyIgnore = [
+              "exit"
+            ];
+          };
+        };
+      }
+      (lib.modules.mkIf cfg.backends.bash.integrations.batpipe.enable
+        {
+          programs.bash.initExtra = ''
+            eval "$(${lib.meta.getExe pkgs.bat-extras.batpipe})"
+          '';
+        })
+      (lib.modules.mkIf cfg.backends.bash.integrations.direnv.enable
+        {
+          programs.direnv = {
+            enableBashIntegration = true;
+          };
+        })
+      (lib.modules.mkIf cfg.backends.bash.integrations.fzf.enable
+        {
+          programs.fzf = {
+            enableBashIntegration = true;
+          };
+        })
+      (lib.modules.mkIf cfg.backends.bash.integrations.lsd.enable
+        {
+          programs.lsd = {
+            enableBashIntegration = true;
+          };
+        })
+      (lib.modules.mkIf cfg.backends.bash.integrations.kitty.enable
+        {
+          programs.kitty = {
+            shellIntegration.enableBashIntegration = true;
+          };
+        })
+      (lib.modules.mkIf cfg.backends.bash.integrations.starship.enable
+        {
+          programs.starship = {
+            enableBashIntegration = true;
+          };
+        })
+      (lib.modules.mkIf cfg.backends.bash.integrations.yazi.enable
+        {
+          programs.bash.initExtra = ''
+            function y() {
+            	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+            	yazi "$@" --cwd-file="$tmp"
+            	IFS= read -r -d \'\' cwd < "$tmp"
+            	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+            	rm -f -- "$tmp"
+            }
+          '';
+
+          programs.yazi = {
+            enableBashIntegration = true;
+          };
+        })
+      (lib.modules.mkIf cfg.backends.bash.integrations.zoxide.enable
+        {
+          programs.zoxide = {
+            enableBashIntegration = true;
+          };
+        })
+    ]);
+  };
+}
---
+++ modules/shell/backends/nushell.nix
@@ -0,0 +1,65 @@
+{
+  flake.modules.homeManager.shell = {
+    config,
+    lib,
+    ...
+  }: let
+    cfg = config.shell;
+  in {
+    options.shell.backends.nushell = {
+      enable = lib.options.mkEnableOption "Whether to manage `nushell`.";
+      integrations = {
+        direnv.enable =
+          lib.options.mkEnableOption "Whether to enable the `direnv` integration for `nushell`."
+          // {
+            default = cfg.integrations.direnv.enable;
+          };
+        starship.enable =
+          lib.options.mkEnableOption "Whether to enable the `starship` integration for `nushell`."
+          // {
+            default = cfg.integrations.starship.enable;
+          };
+        yazi.enable =
+          lib.options.mkEnableOption "Whether to enable the `yazi` integration for `nushell`."
+          // {
+            default = cfg.integrations.yazi.enable;
+          };
+        zoxide.enable =
+          lib.options.mkEnableOption "Whether to enable the `zoxide` integration for `nushell`."
+          // {
+            default = cfg.integrations.zoxide.enable;
+          };
+      };
+    };
+
+    config = lib.modules.mkIf cfg.backends.nushell.enable (lib.modules.mkMerge [
+      {
+        programs.nushell.enable = true;
+      }
+      (lib.modules.mkIf cfg.backends.nushell.integrations.direnv.enable
+        {
+          programs.direnv = {
+            enableNushellIntegration = true;
+          };
+        })
+      (lib.modules.mkIf cfg.backends.nushell.integrations.starship.enable
+        {
+          programs.starship = {
+            enableNushellIntegration = true;
+          };
+        })
+      (lib.modules.mkIf cfg.backends.nushell.integrations.yazi.enable
+        {
+          programs.yazi = {
+            enableNushellIntegration = true;
+          };
+        })
+      (lib.modules.mkIf cfg.backends.nushell.integrations.zoxide.enable
+        {
+          programs.zoxide = {
+            enableNushellIntegration = true;
+          };
+        })
+    ]);
+  };
+}
