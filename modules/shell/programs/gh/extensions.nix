b36224fba4a0067ff274e73457d4397a397c9fe2
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2026-03-17 00:58:39 -0400

    Add gh dendritic module

---
+++ modules/shell/programs/gh/extensions.nix
@@ -0,0 +1,26 @@
+{
+  flake.modules.homeManager.gh = {
+    config,
+    lib,
+    pkgs,
+    ...
+  }: let
+    cfg = config.shell.programs.gh;
+  in {
+    programs = {
+      gh-dash = lib.modules.mkIf cfg.enable {
+        # enable = false;
+        settings = {
+        };
+      };
+
+      gh.extensions = with pkgs; [
+        gh-poi
+        # gh-markdown-preview # needs browser
+        # gh-eco # meh
+        # gh-s
+        # gh-f
+      ];
+    };
+  };
+}
