c32ee5a31b659791acd76af1022379e7058c21bd
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2026-03-17 00:07:09 -0400

    Add diff abstraction

---
+++ modules/shell/programs/diff/backends/delta/enable.nix
@@ -0,0 +1,23 @@
+{
+  flake.modules.homeManager.diff = {lib, ...}: {
+    imports = [
+      (
+        lib.modules.mkAliasOptionModule
+        ["shell" "programs" "diff" "backends" "delta" "enable"]
+        ["programs" "delta" "enable"]
+      )
+      (
+        lib.modules.mkAliasOptionModule
+        ["shell" "programs" "diff" "backends" "delta" "package"]
+        ["programs" "delta" "package"]
+      )
+    ];
+
+    programs.delta.options = {
+      navigate = true;
+      line-numbers = true;
+      hyperlinks = true;
+      side-by-side = true;
+    };
+  };
+}
