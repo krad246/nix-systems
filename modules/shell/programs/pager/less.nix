8551dde2355418494aa1f2de189f0f31a76841ac
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2026-03-17 00:40:52 -0400

    Add pager abstraction

---
+++ modules/shell/programs/pager/less.nix
@@ -0,0 +1,33 @@
+{
+  flake.modules.homeManager.pager = {
+    config,
+    lib,
+    ...
+  }: let
+    cfg = config.shell.programs.pager;
+  in {
+    options.shell.programs.pager.enable = lib.options.mkEnableOption "Whether to enable an augmented `PAGER` program.";
+
+    config = lib.modules.mkIf cfg.enable {
+      programs = {
+        less = {
+          enable = true;
+          config = ''
+          '';
+        };
+
+        lesspipe.enable = lib.modules.mkDefault true;
+      };
+
+      # -I: ignore-case
+      # -F: quit-if-one-screen
+      # -R: raw-control-characters
+      # -K: quit-on-intr
+      # -s: squeeze-blank-lines
+      home.sessionVariables = {
+        LESS = "-IFRKs";
+        PAGER = "less";
+      };
+    };
+  };
+}
