78eeb72f97fec986456e2cdea7b00108d893ef66
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2026-04-09 23:30:59 -0700

    Add flake substituters

--- flake.nix
+++ flake.nix
@@ -3,6 +3,11 @@
 {
   outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} (inputs.import-tree ./modules);
 
+  nixConfig = {
+    substituters = ["https://cache.nixos.org" "https://nix-community.cachix.org"];
+    trusted-public-keys = ["cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
+  };
+
   inputs = {
     agenix = {
       inputs.nixpkgs.follows = "nixpkgs";
