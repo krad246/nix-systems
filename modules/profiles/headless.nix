5b59d9a4512690993af4cbea06696cbc59436cf5
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2026-07-17 19:16:56 -0700

    aaa

--- modules/profiles/headless.nix
+++ modules/profiles/headless.nix
@@ -2,9 +2,8 @@
   flake.modules.nixos.headless = {config, ...}: {
     imports = with self.modules.nixos; [
       base
+      terminfo
     ];
-
-    environment.enableAllTerminfo = true;
 
     home-manager.users.${config.owner.username}.imports = [
       self.modules.homeManager.base
---
+++ modules/profiles/terminfo.nix
@@ -0,0 +1,5 @@
+{
+  flake.modules.nixos.terminfo = {
+    environment.enableAllTerminfo = true;
+  };
+}
