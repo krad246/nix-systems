a9a4a16cbbf602feb9ecfb5ef5cd08e38748cc68
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2026-08-13 00:43:01 -0700

    Document deferred VM closure

--- flakeModules/dendritic/image-modules/vm-nogui.nix
+++ flakeModules/dendritic/image-modules/vm-nogui.nix
@@ -1,3 +1,6 @@
+# Like `importApply`: the outer argument closes over the registered VM module,
+# while this function returns the deferred NixOS module evaluated later.
+# TODO: investigate an import-tree closure bridge built on `importApply`.
 {vm}: {
   imports = [vm];
 
