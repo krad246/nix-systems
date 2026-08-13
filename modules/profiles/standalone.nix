c94e9bf16d0ab071f790c844a0475cdf7330d912
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2026-08-12 23:16:54 -0700

    Expose interactive system profiles

--- modules/profiles/standalone.nix
+++ modules/profiles/standalone.nix
@@ -31,6 +31,7 @@
         standalone
         desktop
         dev
+        interactive
         secrets
       ];
 
--- modules/shell/interactive.nix
+++ modules/shell/interactive.nix
@@ -1,76 +1,100 @@
-{lib, ...}: {
-  flake.modules.homeManager.shell = {
-    config,
-    pkgs,
-    ...
-  }: let
-    cfg = config.shell.profiles.interactive;
-  in {
-    options.shell.profiles.interactive.enable =
-      lib.options.mkEnableOption "Whether this to turn on interactive tools and integrations for the CLI."
-      // {
-        default = true;
-      };
-
-    config = lib.modules.mkIf cfg.enable {
-      shell = {
-        backends = {
-          bash.enable = true;
-          nushell.enable = lib.modules.mkDefault false;
+{
+  self,
+  lib,
+  ...
+}: {
+  flake.modules = {
+    homeManager.shell = {
+      config,
+      pkgs,
+      ...
+    }: let
+      cfg = config.shell.profiles.interactive;
+    in {
+      options.shell.profiles.interactive.enable =
+        lib.options.mkEnableOption "interactive tools and integrations for the CLI"
+        // {
+          default = true;
         };
-        integrations = {
-          bat.enable = true;
-          fzf.enable = lib.modules.mkDefault config.picker.backends.fzf.enable;
-          lsd.enable = true;
-          starship.enable = true;
-          yazi.enable = true;
-          zoxide.enable = true;
-        };
-        programs = {
-          bat.enable = true;
-          bottom.enable = true;
-          diff.enable = true;
-          fd.enable = true;
-          lsd.enable = true;
-          man.enable = true;
-          pager.enable = true;
-          ripgrep.enable = true;
-          starship.enable = true;
-          yazi.enable = true;
-          zoxide.enable = true;
-        };
-      };
 
-      picker.backends.fzf.integrations = {
-        bat.enable = lib.modules.mkDefault true;
-        fd.enable = lib.modules.mkDefault true;
-      };
+      config = lib.modules.mkIf cfg.enable {
+        shell = {
+          backends = {
+            bash.enable = true;
+            nushell.enable = lib.modules.mkDefault false;
+          };
+          integrations = {
+            bat.enable = true;
+            fzf.enable = lib.modules.mkDefault config.picker.backends.fzf.enable;
+            lsd.enable = true;
+            starship.enable = true;
+            yazi.enable = true;
+            zoxide.enable = true;
+          };
+          programs = {
+            bat.enable = true;
+            bottom.enable = true;
+            diff.enable = true;
+            fd.enable = true;
+            lsd.enable = true;
+            man.enable = true;
+            pager.enable = true;
+            ripgrep.enable = true;
+            starship.enable = true;
+            yazi.enable = true;
+            zoxide.enable = true;
+          };
+        };
 
-      home.packages = with pkgs; ([
-          coreutils
-          curl
-          duf
-          dust
-          file
-          gnutar
-          jq
-          procps
-          # safe-rm # FIXME: not useful unless aliasing rm
-          sd
-          tldr
-          tree
-          which
-          unzip
-          zip
-        ]
-        ++ lib.lists.optionals pkgs.stdenv.hostPlatform.isLinux [
-          bashmount
-          psmisc
-          strace
-        ]
-        ++ lib.lists.optionals pkgs.stdenv.hostPlatform.isDarwin [
-          m-cli
-        ]);
+        picker.backends.fzf.integrations = {
+          bat.enable = lib.modules.mkDefault true;
+          fd.enable = lib.modules.mkDefault true;
+        };
+
+        home.packages = with pkgs; ([
+            coreutils
+            curl
+            duf
+            dust
+            file
+            gnutar
+            jq
+            procps
+            # safe-rm # FIXME: not useful unless aliasing rm
+            sd
+            tldr
+            tree
+            which
+            unzip
+            zip
+          ]
+          ++ lib.lists.optionals pkgs.stdenv.hostPlatform.isLinux [
+            bashmount
+            psmisc
+            strace
+          ]
+          ++ lib.lists.optionals pkgs.stdenv.hostPlatform.isDarwin [
+            m-cli
+          ]);
+      };
+    };
+
+    homeManager.interactive = {
+      imports = [self.modules.homeManager.shell];
+
+      shell.profiles.interactive.enable = true;
+    };
+
+    nixos.interactive = {config, ...}: {
+      home-manager.users.${config.owner.username}.imports = [
+        self.modules.homeManager.interactive
+      ];
+    };
+
+    darwin.interactive = {config, ...}: {
+      home-manager.users.${config.owner.username}.imports = [
+        self.modules.homeManager.interactive
+      ];
     };
   };
 }
