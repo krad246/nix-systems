37da89a7b80d88476048e45f10b2e9ead37093c6
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2026-08-13 01:33:07 -0700

    Prove coherent flake library consumers

--- modules/flake/lib.nix
+++ modules/flake/lib.nix
@@ -1,3 +1,30 @@
-{inputs, ...}: {
+{
+  inputs,
+  self,
+  ...
+}: {
   flake.lib = inputs.nixpkgs.lib;
+
+  perSystem = {system, ...}:
+    self.lib.mkMerge [
+      (self.lib.mkIf (system == "aarch64-darwin") {
+        checks.flake-lib-nixbook-pro = let
+          cfg = self.darwinConfigurations.nixbook-pro.config;
+          home = cfg.home-manager.users.${cfg.owner.username};
+        in
+          assert self.lib.trivial.release == inputs.nixpkgs.lib.trivial.release;
+          assert home.home.stateVersion == self.lib.trivial.release;
+            home.home.activationPackage;
+      })
+      (self.lib.mkIf (system == "aarch64-linux") {
+        checks.flake-lib-miniboi = let
+          cfg = self.nixosConfigurations.miniboi.config;
+          home = cfg.home-manager.users.${cfg.owner.username};
+        in
+          assert self.lib.trivial.release == inputs.nixpkgs.lib.trivial.release;
+          assert cfg.system.stateVersion == self.lib.trivial.release;
+          assert home.home.stateVersion == self.lib.trivial.release;
+            cfg.system.build.toplevel;
+      })
+    ];
 }
