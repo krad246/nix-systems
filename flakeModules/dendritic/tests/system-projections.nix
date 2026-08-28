52d4b4d95e1a9c65d7fd9e98389fb498adefa85c
Keerthi Radhakrishnan <condor-janitor0e@icloud.com>
2026-08-28 07:36:19 -0700

    Project named system images

--- flakeModules/dendritic/tests/system-projections.nix
+++ flakeModules/dendritic/tests/system-projections.nix
@@ -2,7 +2,7 @@ let
   flake = builtins.getFlake (toString ../../..);
   linux = flake.nixosConfigurations;
   darwin = flake.darwinConfigurations;
-  artifact = flake.packages.x86_64-linux.generic-headless-interactive-vm-nogui-x86_64-linux;
+  image = flake.packages.x86_64-linux.generic-headless-interactive-vm-nogui-x86_64-linux;
 in
   assert linux ? generic-headless-interactive;
   assert linux ? generic-headless-interactive-dev;
@@ -11,4 +11,4 @@ in
   assert linux.generic-headless-interactive-dev.config.environment.etc.dendritic-variant.text == "dev";
   assert darwin ? nixbook-pro-composed;
   assert darwin.nixbook-pro-composed.config.home-manager.users.krad246.home.sessionVariables.DENDRITIC_SYSTEM_USER == "true";
-  assert artifact.name == "nixos-vm"; true
+  assert image.name == "nixos-vm"; true
